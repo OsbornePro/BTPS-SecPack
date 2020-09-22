a89182e55c473a0c6acc071776e6fc62d49c70e7
tobor <rosborne@osbornepro.com>
2020-09-22 10:04:00 -0600

    Update DNSZoneTransferAlert.ps1

--- Event Alerts/DNSZoneTransferAlert.ps1
+++ Event Alerts/DNSZoneTransferAlert.ps1
@@ -1,17 +1,59 @@
-# This is an alert that should be run in response to DNS Server event ID 6001 to alert IT admins when a DNS zone transfer occurs
-
 $Event = Get-WinEvent -FilterHashtable @{LogName='DNS Server';ID='6001'} -MaxEvents 1
 
-If ($Event -like $Null)
+If ($Event)
 {
+    
+    $Results = $Event | ForEach-Object {
 
-    exit
+            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, Domain, InitiatedBy, DC, Date, Message
+            $Obj.EventID = $_.Id
+            $Obj.Domain = $_.Properties[1].Value
+            $Obj.InitiatedBy = $_.Properties[2].Value
+            $Obj.DC = $_.MachineName
+            $Obj.Date = $_.TimeCreated
+            $Obj.Message = "DNS Zone Transfer has occured"
+            
+            $Obj
 
-}  # End If
-Else
-{
+    }  # End ForEach-Object
 
-    $MailBody = $Event.Message + "`r`n`t" + $Event.TimeGenerated | Format-List -Property * | Out-String
-    Send-Mailmessage -From $From -To $To -Subject "DNS Zone Transfer Occured" -Body $MailBody -SmtpServer $SmtpServer
+    If ($Results)
+    {
 
-}  # End Else
+    $Css = @"
+<style>
+table {
+    font-family: verdana,arial,sans-serif;
+        font-size:11px;
+        color:#333333;
+        border-width: 1px;
+        border-color: #666666;
+        border-collapse: collapse;
+}
+th {
+        border-width: 1px;
+        padding: 8px;
+        border-style: solid;
+        border-color: #666666;
+        background-color: #dedede;
+}
+td {
+        border-width: 1px;
+        padding: 8px;
+        border-style: solid;
+        border-color: #666666;
+        background-color: #ffffff;
+}
+</style>
+"@ # End CSS 
+
+        $PreContent = "<Title>DNS Zone Transfer</Title>"
+        $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
+        $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
+        $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>A DNS Zone transfer has occured. Details are below.<br><br><hr><br><br>" | Out-String
+
+        Send-MailMessage -From $From -To $To -Subject "AD Event: DNS Zone Transfer Occured" -BodyAsHtml -Body "$MailBody" -SmtpServer $SmtpServer
+
+    }  # End If
+
+}  # End Else 
