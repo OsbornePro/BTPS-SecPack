357d0ffd6714521ae4c029869deafd1bfa311c50
tobor <rosborne@osbornepro.com>
2020-09-30 20:24:09 -0600

    Update MaliciousIPChecker.ps1

--- Sysmon/MaliciousIPChecker.ps1
+++ Sysmon/MaliciousIPChecker.ps1
@@ -172,6 +172,155 @@ Function Get-WhoIsLookupInfo {
 }  # End Function Get-WhoIsLookupInfo
 
 
+# REFERENCE https://community.spiceworks.com/scripts/show/2428-powershell-rbl-blacklist-check-with-email-alerts
+Function Invoke-IPBlacklistCheck {
+    [CmdletBinding()]
+        param(
+            [Parameter(
+                Position=0,
+                Mandatory=$True)]  # End Parameter
+            [String[]]$IPAddress)  # End Param
+
+
+    $LogfileExists = Get-Eventlog -List | Where-Object {$_.logdisplayname -eq "BlacklistedIPs"}
+    If (!($LogfileExists))
+    {
+
+        Write-Verbose "Creating MaliciousIPs event log tree"
+        New-EventLog -LogName MaliciousIPs -Source MaliciousIPs
+        Limit-EventLog -LogName "MaliciousIPs" -OverflowAction OverWriteAsNeeded -MaximumSize 64KB
+        
+        Write-Verbose "Creating directory to save log info"
+        New-Item -Path "C:\Program Files\BlacklistedIPs" -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
+
+    }  # End If
+
+    ForEach ($IP in $IPAddress)
+    {
+
+        $BlacklistedOn = @()
+        $ReversedIP = ($IP -Split '\.')[3..0] -Join '.'
+        $BlacklistServers = @(
+            'b.barracudacentral.org'
+            'spam.rbl.msrbl.net'
+            'zen.spamhaus.org'
+            'bl.deadbeef.com'
+            'bl.emailbasura.org'
+            'bl.spamcannibal.org'
+            'bl.spamcop.net'
+            'blackholes.five-ten-sg.com'
+            'blacklist.woody.ch'
+            'bogons.cymru.com'
+            'cbl.abuseat.org'
+            'cdl.anti-spam.org.cn'
+            'combined.abuse.ch'
+            'combined.rbl.msrbl.net'
+            'db.wpbl.info'
+            'dnsbl-1.uceprotect.net'
+            'dnsbl-2.uceprotect.net'
+            'dnsbl-3.uceprotect.net'
+            'dnsbl.ahbl.org'
+            'dnsbl.cyberlogic.net'
+            'dnsbl.inps.de'
+            'dnsbl.njabl.org'
+            'dnsbl.sorbs.net'
+            'drone.abuse.ch'
+            'drone.abuse.ch'
+            'duinv.aupads.org'
+            'dul.dnsbl.sorbs.net'
+            'dul.ru'
+            'dyna.spamrats.com'
+            'dynip.rothen.com'
+            'http.dnsbl.sorbs.net'
+            'images.rbl.msrbl.net'
+            'ips.backscatterer.org'
+            'ix.dnsbl.manitu.net'
+            'korea.services.net'
+            'misc.dnsbl.sorbs.net'
+            'noptr.spamrats.com'
+            'ohps.dnsbl.net.au'
+            'omrs.dnsbl.net.au'
+            'orvedb.aupads.org'
+            'osps.dnsbl.net.au'
+            'osrs.dnsbl.net.au'
+            'owfs.dnsbl.net.au'
+            'owps.dnsbl.net.au'
+            'pbl.spamhaus.org'
+            'phishing.rbl.msrbl.net'
+            'probes.dnsbl.net.au'
+            'proxy.bl.gweep.ca'
+            'proxy.block.transip.nl'
+            'psbl.surriel.com'
+            'rbl.interserver.net'
+            'rdts.dnsbl.net.au'
+            'relays.bl.gweep.ca'
+            'relays.bl.kundenserver.de'
+            'relays.nether.net'
+            'residential.block.transip.nl'
+            'ricn.dnsbl.net.au'
+            'rmst.dnsbl.net.au'
+            'sbl.spamhaus.org'
+            'short.rbl.jp'
+            'smtp.dnsbl.sorbs.net'
+            'socks.dnsbl.sorbs.net'
+            'spam.abuse.ch'
+            'spam.dnsbl.sorbs.net'
+            'spam.spamrats.com'
+            'spamlist.or.kr'
+            'spamrbl.imp.ch'
+            't3direct.dnsbl.net.au'
+            'tor.ahbl.org'
+            'tor.dnsbl.sectoor.de'
+            'torserver.tor.dnsbl.sectoor.de'
+            'ubl.lashback.com'
+            'ubl.unsubscore.com'
+            'virbl.bit.nl'
+            'virus.rbl.jp'
+            'virus.rbl.msrbl.net'
+            'web.dnsbl.sorbs.net'
+            'wormrbl.imp.ch'
+            'xbl.spamhaus.org'
+            'zombie.dnsbl.sorbs.net')
+
+        ForEach ($Server in $BlacklistServers)
+        {
+
+            $FQDN = "$ReversedIP.$Server"
+            Try
+            {
+
+                $Null = [System.Net.Dns]::GetHostEntry($FQDN)
+                $BlacklistedOn += $Server
+
+            }  # End Try
+            Catch 
+            { 
+
+                Continue
+
+            }  # End Catch
+
+        }  # End ForEach
+
+        If ($BlacklistedOn.Count -gt 0)
+        {
+
+            Write-Verbose "Create an event in BlacklistIPs Event Viewer Tree"
+            Foreach ($Item in $BlacklistedOn) 
+            {
+
+                $Message = Write-Output "IP Address was found to be on the following Blacklists: `n" + ($Item  | Out-String -Width 1000)
+                Write-EventLog -LogName MaliciousIPs -Source MaliciousIPs -EntryType Information -EventId 1 -Message $Message
+
+            }  # End ForEach
+
+        }  # End If
+
+    }  # End ForEach
+
+}  # End Function Invoke-IPBlacklistCheck
+
+
 $Results = @()
 $TmpEventFile = "C:\Windows\Temp\SysmonEvents.txt"
 
@@ -189,6 +338,20 @@ ForEach ($IP in $IPList)
 
     }  # End If
 
-    $Results
+    ForEach ($Result in $Results)
+    {
+    
+        $DateLimit = (Get-Date).Year
+        If (($DateLimit - ($Result.CreatedDate).Year) -le 2)
+        {
+        
+            Write-Verbose "Creating an event in MaliciousIPs Event Viewer Tree for a young domain"
+
+            $Message = Write-Output "Domain was found to be less than a year old: `n" + ($Result  | Out-String -Width 1000)
+            Write-EventLog -LogName MaliciousIPs -Source MaliciousIPs -EntryType Information -EventId 2 -Message $Message
+        
+        }  # End If
+    
+    }  # End ForEach
 
 }  # End ForEach  
