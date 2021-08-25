becfae48ee5a9d9e7028d428c4c5cfa8c99d9c96
tobor <rosborne@osbornepro.com>
2021-08-25 16:04:54 -0600

    Update Failed.Username.and.Password.ps1
    Updated to use a Teams Post

--- Account and Password Alerts/Failed.Username.and.Password.ps1
+++ Account and Password Alerts/Failed.Username.and.Password.ps1
@@ -1,56 +1,77 @@
 # This alert is used to alert IT when a failed password attempt occurs on a server
-$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 120000]]]' -MaxEvents 1
+$Events = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 120000]]]' -MaxEvents 1
+$ArrayTable = New-Object -TypeName 'System.Collections.Generic.List[System.Object]'
 
-$Results = $Event | ForEach-Object {
+If ($Events) {
 
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, Device, DC, Type, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.User = $_.Properties[5].Value
-            $Obj.Device = $_.Properties[6].Value
-            $Obj.DC = $_.MachineName
-            $Obj.Type = $_.Properties[12].Value
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "An account failed to log on"
+    $Events | ForEach-Object {
 
-            $Obj
+        $Section = @{
+          activityTitle = "An account failed to log on"
+          activitySubtitle = "$($_.Properties[5].Value)"
+          activityText  = "Failed Login Attempt reported by $($_.MachineName) at $($_.TimeCreated)"
+          activityImage = ""
+	      facts		  = @(
+                @{
+                        name = "EventID: "
+                        value = $_.Id
+                    }
+                @{
+                        name = "User: "
+                        value = $_.Properties[5].Value
+                    },
+                @{
+                        name = "Device: "
+                        value = $_.Properties[6].Value
+                    },
+                @{
+                        name = "DC: "
+                        value = $_.MachineName
+                    },
+                @{
+                        name = "AuthType: "
+                        value = $_.Properties[12].Value
+                    },
+                @{
+                        name = "Date: "
+                        value = $_.TimeCreated.ToString()
+                    },
+                @{
+                        name = "Message"
+                        value = "An account failed to log on"
+                    }
+            )  # End Facts
 
-}  # End ForEach-Object
+	    }  # End Section
+
+	    $ArrayTable.Add($Section)
+
+    }  # End ForEach-Object
+
+    $Body = ConvertTo-Json -Depth 8 @{
+	    title = "Failed Login Notification"
+	    text  = "There are $($ArrayTable.Count) failed login attempts"
+	    sections = $ArrayTable
+        potentialAction =   @(
+                        @{
+                            '@context'  = 'http://schema.org'
+                            '@type' = 'ViewAction'
+                            name = 'How to Reset a User Password'
+                            target = @("https://docs.microsoft.com/en-us/powershell/module/activedirectory/set-adaccountpassword?view=windowsserver2019-ps")
+                        },
+                        @{
+                            '@context'  = 'http://schema.org'
+                            '@type' = 'ViewAction'
+                            name = 'Elastichsearch for Correlation Events'
+                            target = @("SIEM TOOL LINK")
+                        }
+                    )  # End Potential Actions
+    }  # End Body
 
 
-If ($Results) {
-
-    $Css = @"
-<style>
-table {
-    font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
-}
-th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
-}
-td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
-}
-</style>
-"@ # End CSS
-
-    $PreContent = "<Title>NOTIFICATION: A Password Change Has Been Attempted</Title>"
-    $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a user whose password was changed.<br><br><hr><br><br>" | Out-String
-
-    Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Failed UserName and Password $env:COMPUTERNAME" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+    $WebhookUrl = 'WEBHOOK_URL_REPLACE'
+    
+    # Post the JSON Array Object to the Webhook Connector URI
+    Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body $Body -ContentType 'application/json'
 
 }  # End If
