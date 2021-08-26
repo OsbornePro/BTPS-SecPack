00fd796cea1fe2a2241a809fd539e7dfc4cec282
tobor <rosborne@osbornepro.com>
2021-08-25 22:41:16 -0600

    Update NewComputerAlert.ps1
    Updated to use Teams Post instead of Email Alert

--- Event Alerts/NewComputerAlert.ps1
+++ Event Alerts/NewComputerAlert.ps1
@@ -1,4 +1,5 @@
 $Events = Get-WinEvent -FilterHashtable @{Logname='Security';Id=4741;StartTime=(Get-Date).AddHours("-1")} | Select-Object -First 1
+
 $Results = $Events | ForEach-Object {
 
             $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, UserName, DomainName, MachineName, Date, Message
@@ -8,45 +9,43 @@ $Results = $Events | ForEach-Object {
             $Obj.MachineName = $_.Properties[26].Value
             $Obj.Date = $_.TimeCreated
             $Obj.Message = "A new computer object was created"
-
+            
             $Obj
 
 }  # End ForEach-Object
 
+# TEAMS POST
 If ($Results) {
 
-    $Css = @"
-<style>
-table {
-    font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
-}
-th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
-}
-td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
-}
-</style>
-"@ # End CSS
+    $Username = $Obj.Username
+    $EventID = $Obj.EventID
+    $Domain = $Obj.DomainName
+    $Machine = $Obj.MachineName
+    $AuthType = $Obj.Type
+    $Date = $Obj.Date.ToLongDateString()
+    $Message = $Obj.Message
+    $WebhookUrl = 'WEBHOOK_URL_REPLACE'
+    $Body = ConvertTo-Json -Depth 8 @{
+        title = "New Computer Joined Domain"
+        text = "
+        Event ID = $EventID
+        Username = $Username 
+        Domain   = $Domain
+        Device   = $Machine
+        Time     = $Date
+        Message  = $Message"
+        summary = 'New Computer Joined Domain'
+        potentialAction =   @(
+                                @{
+                                    '@context'  = 'http://schema.org'
+                                    '@type' = 'ViewAction'
+                                    name = 'Elasticsearch for Correlation Events'
+                                    target = @("SIEM TOOL LINK")
+                                }
+                            ) 
+    }  # End Body
 
-    $PreContent = "<Title>NOTIFICATION: A New Computer Object was Added to Domain</Title>"
-    $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a new computer object that was added to the domain.<br><br><hr><br><br>" | Out-String
-
-    Send-MailMessage -From FromEamil -To ToEmail -Subject "AD Event: New Computer Added" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+    # Post the JSON Array Object to the Webhook Connector URI
+    Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body $Body -ContentType 'application/json'
 
 }  # End If
