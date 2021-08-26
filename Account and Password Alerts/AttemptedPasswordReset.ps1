1b5774998900114dae44cb216cd33a62dfd9b055
tobor <rosborne@osbornepro.com>
2021-08-25 22:32:00 -0600

    Update AttemptedPasswordReset.ps1
    Updated to use Teams Post instead of Email Alert

--- Account and Password Alerts/AttemptedPasswordReset.ps1
+++ Account and Password Alerts/AttemptedPasswordReset.ps1
@@ -1,54 +1,67 @@
 # Alert IT when a users password is changed by another account
 $Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4724 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1
+$ArrayTable = New-Object -TypeName 'System.Collections.Generic.List[System.Object]'
 
-$Results = $Event | ForEach-Object {
+If ($Events) {
 
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, EffectedUser, ExecutingUser, MachineName, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.EffectedUser = $_.Properties[0].Value
-            $Obj.ExecutingUser = $_.Properties[4].Value
-            $Obj.MachineName = $_.MachineName
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "An attempt was made to change an account password"
+    $Events | ForEach-Object {
 
-            $Obj
+        $Section = @{
+          activityTitle = "Admin Changed a Users Password"
+          activitySubtitle = "$($_.Properties[0].Value)"
+          activityText  = "$($_.Properties[4].Value) changed $($_.Properties[0].Value) password at $($_.TimeCreated.ToString())"
+          activityImage = ""
+	      facts		  = @(
+                @{
+                        name = "EventID: "
+                        value = $_.Id
+                    }
+                @{
+                        name = "Effected User: "
+                        value = $_.Properties[0].Value
+                    },
+                @{
+                        name = "Executing User: "
+                        value = $_.Properties[4].Value
+                    },
+                @{
+                        name = "DC: "
+                        value = $_.MachineName
+                    },
+                @{
+                        name = "Date: "
+                        value = $_.TimeCreated.ToString()
+                    },
+                @{
+                        name = "Message"
+                        value = "Admin changed a users password"
+                    }
+            )  # End Facts
 
-}  # End ForEach-Object
+	    }  # End Section
 
-If ($Results) {
+	    $ArrayTable.Add($Section)
 
-    $Css = @"
-<style>
-table {
-    font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
-}
-th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
-}
-td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
-}
-</style>
-"@ # End CSS
+    }  # End ForEach-Object
 
-    $PreContent = "<Title>NOTIFICATION: A Password Change Has Been Attempted on one account from a different account</Title>"
-    $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a user whose password was attempted to be changed by another user.<br><br><hr><br><br>" | Out-String
+    $Body = ConvertTo-Json -Depth 8 @{
+	    title = "Admin Changed a Users Password"
+	    text  = "There are $($ArrayTable.Count) passwords changed by $($_.Properties[4].Value)"
+	    sections = $ArrayTable
+        potentialAction =   @(
+                        @{
+                            '@context'  = 'http://schema.org'
+                            '@type' = 'ViewAction'
+                            name = 'SIEM for Correlation Events'
+                            target = @("SIEM TOOL LINK")
+                        }
+                    )  # End Potential Actions
+    }  # End Body
 
-    Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: User Attempted to Change Other Users Password" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+
+    $WebhookUrl = 'WEBHOOK_URL_REPLACE'
+    
+    # Post the JSON Array Object to the Webhook Connector URI
+    Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body $Body -ContentType 'application/json'
 
 }  # End If
