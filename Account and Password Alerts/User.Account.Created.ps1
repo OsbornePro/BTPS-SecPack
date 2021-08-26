d2bcbdf431a1e056784c0555db18a229c2583ec5
tobor <rosborne@osbornepro.com>
2021-08-25 22:35:08 -0600

    Update User.Account.Created.ps1
    Updated to use Teams Post instead of Email Alert

--- Account and Password Alerts/User.Account.Created.ps1
+++ Account and Password Alerts/User.Account.Created.ps1
@@ -1,54 +1,73 @@
 # Alert IT when a user account is created
 $Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4720 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1
+$ArrayTable = New-Object -TypeName 'System.Collections.Generic.List[System.Object]'
 
-$Results = $Event | ForEach-Object {
+If ($Events) {
 
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, SID, MachineName, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.User = $_.Properties[0].Value
-            $Obj.MachineName = $_.MachineName
-            $Obj.SID = $_.Properties[2].Value
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "A user account was created"
+    $Events | ForEach-Object {
 
-            $Obj
+        $Section = @{
+          activityTitle = "New User Account Created"
+          activitySubtitle = "$($_.Properties[0].Value)"
+          activityText  = "New user account $($_.Properties[0].Value) created at $($_.TimeCreated.ToString())"
+          activityImage = ""
+	      facts		  = @(
+                @{
+                        name = "EventID: "
+                        value = $_.Id
+                    }
+                @{
+                        name = "User: "
+                        value = $_.Properties[0].Value
+                    },
+                @{
+                        name = "SID: "
+                        value = $_.Properties[2].Value
+                    },
+                @{
+                        name = "Device Name: "
+                        value = $_.MachineName
+                    },
+                @{
+                        name = "Date: "
+                        value = $_.TimeCreated.ToString()
+                    },
+                @{
+                        name = "Message"
+                        value = "New User Account Created"
+                    }
+            )  # End Facts
 
-}  # End ForEach-Object
+	    }  # End Section
 
-If ($Results) {
+	    $ArrayTable.Add($Section)
 
-    $Css = @"
-<style>
-table {
-    font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
-}
-th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
-}
-td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
-}
-</style>
-"@ # End CSS
+    }  # End ForEach-Object
 
-    $PreContent = "<Title>NOTIFICATION: A User Account has been Created</Title>"
-    $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a newly created user account.<br><br><hr><br><br>" | Out-String
+    $Body = ConvertTo-Json -Depth 8 @{
+	    title = "User Account Created"
+	    text  = "There are $($ArrayTable.Count) new user accounts created"
+	    sections = $ArrayTable
+        potentialAction =   @(
+                        @{
+                            '@context'  = 'http://schema.org'
+                            '@type' = 'ViewAction'
+                            name = 'Elasticsearch for Correlation Events'
+                            target = @("https://selenium.usav.org:5601/")
+                        },
+                        @{
+                            '@context'  = 'http://schema.org'
+                            '@type' = 'ViewAction'
+                            name = 'SIEM for Correlation Events'
+                            target = @("SIEM TOOL LINK")
+                        }
+                    )  # End Potential Actions
+    }  # End Body
 
-   Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Account Created" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -UseSsl -Port 587 -Credential $Credential
+
+    $WebhookUrl = 'WEBHOOK_URL_REPLACE'
+    
+    # Post the JSON Array Object to the Webhook Connector URI
+    Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body $Body -ContentType 'application/json'
 
 }  # End If
