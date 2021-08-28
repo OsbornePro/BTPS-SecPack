a6afe33e9f6365e95ccf3c13ffe4481df3386ebe
tobor <rosborne@osbornepro.com>
2021-08-28 12:53:23 -0600

    Update Watch-PortScan.ps1
    Updated to use a Teams alert instead of email

--- Local Port Scan Monitor/Watch-PortScan.ps1
+++ Local Port Scan Monitor/Watch-PortScan.ps1
@@ -620,9 +620,7 @@ Function Watch-PortScan {
     While ($True) {
 
         Write-Verbose "Checking log entries for scanning attempts"
-
         $IPList = Get-ValidIPAddressFromString -Path "$LogFile"
-
         $ArrayList = New-Object -TypeName System.Collections.ArrayList(,$IPList)
 
         Write-Verbose "Removing excluded addresses from radar"
@@ -645,6 +643,9 @@ Function Watch-PortScan {
 
             Write-Output "Checking $SourceAddress"
 
+            # 4200000 miliseconds is 70 minutes
+            # 2400000 miliseconds is 40 minutes
+            # 1200000 miliseconds is 20 minutes
             $XPath = "*[System[EventID=5156 and TimeCreated[timediff(@SystemTime) <= 120000]] and EventData[Data[@Name='SourceAddress']='$SourceAddress']]"
             $Events = Get-WinEvent -LogName Security -FilterXPath $XPath -ErrorAction SilentlyContinue
 
@@ -716,44 +717,82 @@ Function Watch-PortScan {
 
                 Write-Verbose "Alert Limit Has Been Reached!"
                 $ScanFound = $True
-
                 If ($EmailAlert.IsPresent) {
 
-                    Write-Verbose "Alerting admins"
-                    $Css = @"
-<style>
-table {
-    font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
-}
-th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
-}
-td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
-}
-</style>
-"@
+                    $ArrayTable = New-Object -TypeName 'System.Collections.Generic.List[System.Object]'
 
-                    $TableInfo = $EntryObjList | Select-Object -Property Hostname,TimeCreated,SourceAddress,Destination,SourcePort,DestinationPort,Protocol
-                    $PreContent = "<Title>Port Scan Monitor Detections</Title>"
-                    $NoteLine = "$(Get-Date -format 'MM/dd/yyyy HH:mm:ss')"
-                    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-                    $MailBody = $TableInfo | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "=======================================================<br> PORT SCAN DETECTED: $env:COMPUTERNAME <br>=======================================================<br><br>SUMMARY: <br>A possible port scan was discovered on $env:COMPUTERNAME.<br>" | Out-String
+                    If ($EntryObjList) {
 
-                    Send-MailMessage -To ToEmail -From FromEmail -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587 -Priority High -Subject "ALERT: Attempted Port Scan $env:COMPUTERNAME" -BodyAsHtml -Body $MailBody
+                        $EntryObjList | ForEach-Object {
+
+                            $Section = @{
+                              activityTitle = "An account failed to log on"
+                              activitySubtitle = "$($_.Hostname)"
+                              activityText  = "Failed Login Attempt reported by $($_.Hostname) at $($_.TimeCreated)"
+                              activityImage = ""
+	                          facts		  = @(
+                                    @{
+                                            name = "Detected On: "
+                                            value = $_.Hostname
+                                        },
+                                    @{
+                                            name = "Source Address: "
+                                            value = $_.SourceAddress
+                                        },
+                                    @{
+                                            name = "Source Port: "
+                                            value = $_.SourcePort
+                                        },
+                                    @{
+                                            name = "Destination: "
+                                            value = $_.Destination
+                                        },
+                                    @{
+                                            name = "Destination Port: "
+                                            value = $_.DestinationPort
+                                        },
+                                    @{
+                                            name = "Protocol: "
+                                            value = $_.Protocol
+                                        },
+                                    @{
+                                            name = "Message"
+                                            value = "Possible Port Scan Detected"
+                                        }
+                                )  # End Facts
+
+	                        }  # End Section
+
+	                        $ArrayTable.Add($Section)
+
+                        }  # End ForEach-Object
+
+                        $Body = ConvertTo-Json -Depth 8 @{
+	                        title = "Possible Port Scan Detected"
+	                        text  = "There were $($ArrayTable.Count) connections to $($EntryObjList[0].Hostname)in the last 20 minutes from $($EntryObjList[0].SourceAddress)"
+	                        sections = $ArrayTable
+                            potentialAction =   @(
+                                            @{
+                                                '@context'  = 'http://schema.org'
+                                                '@type' = 'ViewAction'
+                                                name = 'NIST Description'
+                                                target = @("https://csrc.nist.gov/glossary/term/port_scan")
+                                            },
+                                            @{
+                                                '@context'  = 'http://schema.org'
+                                                '@type' = 'ViewAction'
+                                                name = 'SIEM for Correlation Events'
+                                                target = @("SIEM TOOL LINK")
+                                            }
+                                        )  # End Potential Actions
+                        }  # End Body
+
+
+                        $WebhookUrl = 'WEBHOOK_URL_REPLACE'
+                        # Post the JSON Array Object to the Webhook Connector URI
+                        Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body $Body -ContentType 'application/json'
+
+                    }  # End If
 
                 }  # End If
 
@@ -815,76 +854,3 @@ Enable-FirewallLogging -Path $LogPath
 
 Write-Output "[*] Monitoring for port scans on localhost"
 Watch-PortScan -LogFile "$LogPath\domainfw.log" -ExcludeAddresses 'vulnscanner.osbornepro.com','networkmonitor.osbornepro.com' -Limit 5 -EmailAlert -Tail 8000
-
-# SIG # Begin signature block
-# MIIM9AYJKoZIhvcNAQcCoIIM5TCCDOECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQURjRZzxHnF8VWLG75QHYzk27R
-# bEGgggn7MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UE
-# BhMCVVMxEDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAY
-# BgNVBAoTEUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290
-# IENlcnRpZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMx
-# MDUwMzA3MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMw
-# EQYDVQQHEwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEt
-# MCsGA1UECxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMw
-# MQYDVQQDEypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0g
-# RzIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYusw
-# ZLiBCGzDBNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz
-# 6ojcnqOvK/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am
-# +GZHY23ecSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1g
-# O7GyQ5HYpDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQW
-# OlDxSq7neTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB
-# 0lL7AgMBAAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIB
-# BjAdBgNVHQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqF
-# BxBnKLbv9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhho
-# dHRwOi8vb2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDov
-# L2NybC5nb2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0g
-# ADAzMDEGCCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9z
-# aXRvcnkvMA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyI
-# BslQj6Zz91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwl
-# TxFWMMS2RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKo
-# cyQetawiDsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1
-# KrKQ0U11GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkK
-# rqeKM+2xLXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDABMIIFIzCC
-# BAugAwIBAgIIXIhNoAmmSAYwDQYJKoZIhvcNAQELBQAwgbQxCzAJBgNVBAYTAlVT
-# MRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQHEwpTY290dHNkYWxlMRowGAYDVQQK
-# ExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UECxMkaHR0cDovL2NlcnRzLmdvZGFk
-# ZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQDEypHbyBEYWRkeSBTZWN1cmUgQ2Vy
-# dGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwHhcNMjAxMTE1MjMyMDI5WhcNMjExMTA0
-# MTkzNjM2WjBlMQswCQYDVQQGEwJVUzERMA8GA1UECBMIQ29sb3JhZG8xGTAXBgNV
-# BAcTEENvbG9yYWRvIFNwcmluZ3MxEzARBgNVBAoTCk9zYm9ybmVQcm8xEzARBgNV
-# BAMTCk9zYm9ybmVQcm8wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJ
-# V6Cvuf47D4iFITUSNj0ucZk+BfmrRG7XVOOiY9o7qJgaAN88SBSY45rpZtGnEVAY
-# Avj6coNuAqLa8k7+Im72TkMpoLAK0FZtrg6PTfJgi2pFWP+UrTaorLZnG3oIhzNG
-# Bt5oqBEy+BsVoUfA8/aFey3FedKuD1CeTKrghedqvGB+wGefMyT/+jaC99ezqGqs
-# SoXXCBeH6wJahstM5WAddUOylTkTEfyfsqWfMsgWbVn3VokIqpL6rE6YCtNROkZq
-# fCLZ7MJb5hQEl191qYc5VlMKuWlQWGrgVvEIE/8lgJAMwVPDwLNcFnB+zyKb+ULu
-# rWG3gGaKUk1Z5fK6YQ+BAgMBAAGjggGFMIIBgTAMBgNVHRMBAf8EAjAAMBMGA1Ud
-# JQQMMAoGCCsGAQUFBwMDMA4GA1UdDwEB/wQEAwIHgDA1BgNVHR8ELjAsMCqgKKAm
-# hiRodHRwOi8vY3JsLmdvZGFkZHkuY29tL2dkaWcyczUtNi5jcmwwXQYDVR0gBFYw
-# VDBIBgtghkgBhv1tAQcXAjA5MDcGCCsGAQUFBwIBFitodHRwOi8vY2VydGlmaWNh
-# dGVzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMAgGBmeBDAEEATB2BggrBgEFBQcB
-# AQRqMGgwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmdvZGFkZHkuY29tLzBABggr
-# BgEFBQcwAoY0aHR0cDovL2NlcnRpZmljYXRlcy5nb2RhZGR5LmNvbS9yZXBvc2l0
-# b3J5L2dkaWcyLmNydDAfBgNVHSMEGDAWgBRAwr0njsw0gzCiM9f7bLPwtCyAzjAd
-# BgNVHQ4EFgQUkWYB7pDl3xX+PlMK1XO7rUHjbrwwDQYJKoZIhvcNAQELBQADggEB
-# AFSsN3fgaGGCi6m8GuaIrJayKZeEpeIK1VHJyoa33eFUY+0vHaASnH3J/jVHW4BF
-# U3bgFR/H/4B0XbYPlB1f4TYrYh0Ig9goYHK30LiWf+qXaX3WY9mOV3rM6Q/JfPpf
-# x55uU9T4yeY8g3KyA7Y7PmH+ZRgcQqDOZ5IAwKgknYoH25mCZwoZ7z/oJESAstPL
-# vImVrSkCPHKQxZy/tdM9liOYB5R2o/EgOD5OH3B/GzwmyFG3CqrqI2L4btQKKhm+
-# CPrue5oXv2theaUOd+IYJW9LA3gvP/zVQhlOQ/IbDRt7BibQp0uWjYaMAOaEKxZN
-# IksPKEJ8AxAHIvr+3P8R17UxggJjMIICXwIBATCBwTCBtDELMAkGA1UEBhMCVVMx
-# EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT
-# EUdvRGFkZHkuY29tLCBJbmMuMS0wKwYDVQQLEyRodHRwOi8vY2VydHMuZ29kYWRk
-# eS5jb20vcmVwb3NpdG9yeS8xMzAxBgNVBAMTKkdvIERhZGR5IFNlY3VyZSBDZXJ0
-# aWZpY2F0ZSBBdXRob3JpdHkgLSBHMgIIXIhNoAmmSAYwCQYFKw4DAhoFAKB4MBgG
-# CisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcC
-# AQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYE
-# FLq48ATAXtIk6AWchYpx3IRqXfa2MA0GCSqGSIb3DQEBAQUABIIBAL7811C38YvQ
-# 1Nr/uuYQOTACbPHd8erhTM/cpTBt0ehiZKaZaM/quEt/2mIcvpRciPGQN1GNjNP0
-# /OnpQ4o2f3KmtRLn7y5W9qlrPyp671TtbesW+hw6mKIY6tE+okTs0AXOeotgGX8j
-# yJnLsHORZqWE2fLL/0NpdoP6fTXl1gtF0qdqlTvy0qPLraaw3Ccqm1QZaA1i6tJ/
-# K8IQPjUN5qogwoXjNbuXWaFt9C1YJDcNAUX3vnfTAA1dgVCg8tCeSbbPORXpFp9G
-# H6Tswa8Ss9K7K+mFenRDxwtaaNfrrFKDIBb5z30PJEM9IVzkRB6GQS7Q0Ol5a3Xv
-# 8xpGrddIer4=
-# SIG # End signature block
