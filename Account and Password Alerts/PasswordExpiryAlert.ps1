e90b865be78365980812077b1d0beaebdcae78a7
tobor <rosborne@osbornepro.com>
2021-09-02 09:43:56 -0600

    Update PasswordExpiryAlert.ps1

--- Account and Password Alerts/PasswordExpiryAlert.ps1
+++ Account and Password Alerts/PasswordExpiryAlert.ps1
@@ -1,6 +1,5 @@
 # Alerts IT admins and the users who have expiring or expired passwords. This needs to be run on a Domain Controller and works best when set up as a task
 
-# Global variables
 [Int32]$MaxPassAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days
 $RootDSE = Get-ADRootDSE -Server $env:USERDNSDOMAIN
 $PasswordPolicy = Get-ADObject $RootDSE.defaultNamingContext -Property minPwdAge, maxPwdAge, minPwdLength, pwdHistoryLength, pwdProperties
@@ -14,7 +13,7 @@ $Policy =  $PasswordPolicy | Select @{n="PolicyType";e={"Password"}},`
                                   8 {"Passwords can be simple, and the administrator account can be locked out"}
                                   9 {"Passwords must be complex, and the administrator account can be locked out"}
                                   Default {$_.pwdProperties}}}}
-$PolicyString = "Max Password Age: " + ($Policy.maxPwdAge).ToString() + "`nPassword History: " + ($Policy.pwdHistoryLength).ToString() + "`nMinimum Password Length: " + ($Policy.minPwdLength).ToString()
+$PolicyString = "Max Password Age: $($Policy.maxPwdAge)<br>`nPassword History: $($Policy.pwdHistoryLength)<br>`nMinimum Password Length: $($Policy.minPwdLength)"
 [String]$EnvDomain = ([System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()).Name
 [DateTime]$TodaysDate = Get-Date
 [Array]$UserDetails = Get-ADUser -Filter { Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties * | Select-Object -Property "Displayname","Mail", @{l="ExpiryDate";e={$_.PasswordLastSet.AddDays($MaxPassAge)}}
@@ -50,6 +49,8 @@ td {
 ForEach ($Users in $UserDetails) {
 
     $ExpirationDate = $Users.ExpiryDate
+    $DaysLeft = ($ExpirationDate - (Get-Date)).Days | Out-String
+    
     If ($ExpirationDate -ge $TodaysDate) {
 
         $ExpiredPasswords += $Users
@@ -58,7 +59,18 @@ ForEach ($Users in $UserDetails) {
         $PreContent1 = "<Title>ALERT: Password Has Expired</Title>"
         $NoteLine1 = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss') from IT as a friendly reminder."
         $PostContent1 = "<br><p><font size='2'><i>$NoteLine1</i></font>"
-        $MailBody1 = $Users | ConvertTo-Html -Head $Css -PostContent $PostContent1 -PreContent $PreContent1 -Body "Attention $ToWhom, <br><br>If you have received this email your sign in password has expired. <br><br>You can reset your password using the following link: <a href='https://account.activedirectory.windowsazure.com/ChangePassword.aspx?BrandContextID=O365&ruO365='>HERE</a> <br><br>If you are in the office on a company device press Ctrl + Alt + Del and click the Change Password button. If you are using the VPN you will need to connect to the VPN before changing your password. <br><br>$PolicyString<br><br><hr><br>" | Out-String
+        $MailBody1 = $Users | ConvertTo-Html -Head $Css -PostContent $PostContent1 -PreContent $PreContent1 -Body "Attention $ToWhom, <br>
+<br>
+If you have received this email your sign in password has expired.<br>
+<br>
+You can reset your password using the following link: <a href='https://account.activedirectory.windowsazure.com/ChangePassword.aspx?BrandContextID=O365&ruO365='>HERE</a><br>
+<br>If you are in the office on a company device press <strong>(Ctrl + Alt + Del)</strong> and click the <strong>`"Change Password`"</strong> button. If you are using the VPN you will need to connect to the VPN before changing your password. <br>
+<br>
+<strong>Password Policy</strong><br>
+$PolicyString<br>
+<br>
+<hr>
+<br>" | Out-String
 
         # Alerts IT by sending an email
         $From1 = $Users.Mail | Out-String
@@ -71,7 +83,6 @@ ForEach ($Users in $UserDetails) {
         Catch {
 
             Send-MailMessage -From FromEmail -To $From1 -Subject "ACTION REQUIRED: Your Password Has Expired" -BodyAsHtml -Body $MailBody1 -SmtpServer UseSmtpServer -Priority High -UseSSL -Port 587 -Credential $Credential
-
             Send-MailMessage -From FromEmail -To ToEmail -Subject "Forward This Email Alert to $From1. Auto Send Failed" -BodyAsHtml -Body $MailBody1 -SmtpServer UseSmtpServer -UseSSL -Port 587 -Credential $Credential
 
         } # End Catch
@@ -86,7 +97,27 @@ ForEach ($Users in $UserDetails) {
         $PreContent = "<Title>Password Expiring in 15 days or less</Title>"
         $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss') from IT as a friendly reminder."
         $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-        $MailBody = $Users | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "Attention $ToWho, <br><br>If you have received this email your password is expiring in 15 days or less. Reset your password before it expires. <br><br>$PolicyString<br><br>If you are on your home internet you can change your password by performing the following steps. <br><br>    <h4>Change Password From Home while connected to the VPN: </h4><strong>1.)</strong> Connect to the VPN. Enter your username and non-expired password. If you are not connected to a VPN or in the office your password change will not take effect and it will cause issues for you. <br>    <strong>2.)</strong> Press Ctrl+Alt+Del and select the 'Change Password' Button. <br>    <strong>3.)</strong> Enter a new password. Your new password needs to be at least 12 characters long and contain a lowercase letter, uppercase letter, and a number or special character. It can also not be one of the top 50 most commonly used passwords. <br><br>If you are changing your password from your desktop or a laptop that does not need to connect to the VPN because it is connected to DirectAccess, the previous rules apply with the execption of Step 1. Do not connect to the VPN on your desktop or a laptop already connected to DirectAccess as you are already on our network. <br><br><strong>NOTE:</strong> Be sure to sign into your laptop while you are in the office after you have changed your password. This is to ensure the laptop is aware your password has changed before you take it home. <br><br><strong>You are also able to change your password without connecting to the VPN at the following link: <a href='https://account.activedirectory.windowsazure.com/ChangePassword.aspx?BrandContextID=O365&ruO365='>HERE: Change Password Link</a> <br><hr><br>" | Out-String
+        $MailBody = $Users | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "Attention $ToWho, <br>
+<br>
+Your password <strong>EXPIRES IN $DaysLeft DAYS</strong> on <strong>$($ExpirationDate.ToLongDateString())</strong>. Reset your password before it expires.<br>
+<br>
+<a href=`"https://haveibeenpwned.com/Passwords`">Verify Your New Password Has Never Been Exposed</a><br>
+<a href=`"https://bitwarden.com/password-strength/`">Password Strength Checker</a><br>
+<br>
+<strong>Password Policy</strong><br>
+$PolicyString<br>
+<br>
+<h4>How Do I Change My Password?</h4>
+<ol>
+    <li>Make sure you can access a company shared drive. If you can view the contents of a company network share you can change your password</li>
+    <li>Come up with a password following the rules of our password policy above</li>
+    <li>Press <strong>Ctrl+Alt+Del</strong> and click the `"<strong>Change Password</strong>`" Button</li>
+    <li>Enter your current password on the first line and your new password on the second and third lines</li>
+</ol>
+<h4>How Do I Change my Password Wihtout a Computer?</h4>
+ <strong>You are able to change your password at the following link: <a href='https://account.activedirectory.windowsazure.com/ChangePassword.aspx?BrandContextID=O365&ruO365='>HERE: Change Password Link</a> <br>
+ <br>
+<hr><br>" | Out-String
 
         $From = $Users.Mail | Out-String
 
@@ -98,7 +129,6 @@ ForEach ($Users in $UserDetails) {
         Catch {
 
             Send-MailMessage -From FromEmail -To $From1 -Subject "ACTION REUQIRED: Your $EnvDomain Password is Expiring Soon" -BodyAsHtml -Body $MailBody -SmtpServer UseSmptServer -Priority Normal -UseSSL -Port 587 -Credential $Credential
-
             Send-MailMessage -From FromEmail -To ToEmail -Subject "Forward This email to $From1. Auto Send Failed" -BodyAsHtml -Body $MailBody -SmtpServer UseSmptServer -UseSSL -Port 587 -Credential $Credential
 
         } # End Catch
