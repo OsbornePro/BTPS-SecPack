399075c5c62ec273080fecda0d40afe4d79fa1ed
tobor <rosborne@osbornepro.com>
2023-07-07 09:12:24 -0600

    Update Set-SecureFilePermission.ps1

--- Hardening Cmdlets/Set-SecureFilePermission.ps1
+++ Hardening Cmdlets/Set-SecureFilePermission.ps1
@@ -22,7 +22,16 @@ Define the local path to a file you want the permissions changed on. Modifying p
 This parameter defines remote devices that have a file on them you want the permissions changed on. Separate multiple values with a comma
 
 .PARAMETER UseSSL
-This parameter is used to define using WinRM over HTPS when the ComputerName switch parameter is a remote device
+When connecting to remote device use an SSL encrypted connection
+
+.PARAMETER SkipCACheck
+When connecting to remote device skip certificate Root CA verification
+
+.PARAMETER SkipCNCheck
+When connecting to remote device skip certificate canonical name (CN) comparisson check
+
+.PARAMETER SkipRevocationCheck
+When connecting to remote device skip certificate revocation check
 
 
 .EXAMPLE
@@ -30,14 +39,13 @@ Set-SecureFilePermissions -Username 'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrato
 # This example gives SYSTEM, Administrators, Network Configuration Operators, MpsSvc exclusive access to secretfile.txt and sets the Administrators group as the owner
 
 .EXAMPLE
-Set-SecureFilePermissions -Username 'NT AUTHORITY\SYSTEM','BUILTIN\Administrators' -Path "C:\Temp\derp.log" -Owner 'BUILTIN\SYSTEM' -ComputerName 10.0.0.1 -UseSSL
-# This example gives administrators and system permissions to the derp.log file and makes SYSTEM the owner on the remote device 10.0.0.1 using a WinRM over HTTPS connection
+Set-SecureFilePermissions -Username 'NT AUTHORITY\SYSTEM','BUILTIN\Administrators' -Path "C:\Temp\derp.log" -Owner 'BUILTIN\SYSTEM' -ComputerName 10.0.0.1
+# This example gives administrators and system permissions to the derp.log file and makes SYSTEM the owner on the remote device 10.0.0.1
 
 .EXAMPLE
 $Files = Get-ChildItem -Path $env:USERPROFILE\Documents\Scripts -Recurse -Filter *.ps1
 $Files | ForEach-Object { Set-SecureFilePermissions -Username 'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'CONTOSO\Mike' -Path $_.FullName -Owner 'CONTOSO\Mike' -Verbose }
 # This example sets SYSTEM, Administrators, and Mike to have permissions to any ps1 files in the directory defined and sets Mike as the owner.
-
 
 .NOTES
 Author: Robert H. Osborne
@@ -46,7 +54,7 @@ Contact: rosborne@osbornepro.com
 
 
 .INPUTS
-System.String, System.Array
+System.String[]
 
 
 .OUTPUTS
@@ -55,96 +63,138 @@ None
 
 .LINK
 https://osbornepro.com
-https://btpssecpack.osbornepro.com
-https://encrypit.osbornepro.com
 https://writeups.osbornepro.com
-https://github.com/OsbornePro
 https://github.com/tobor88
+https://github.com/OsbornePro
 https://gitlab.com/tobor88
 https://www.powershellgallery.com/profiles/tobor
 https://www.linkedin.com/in/roberthosborne/
 https://www.credly.com/users/roberthosborne/badges
 https://www.hackthebox.eu/profile/52286
 #>
-    [CmdletBinding(DefaultParameterSetName="Local")]
-        param(
-            [Parameter(
-                Mandatory=$True,
-                ValueFromPipeline=$False,
-                HelpMessage="`n[H] Add a user or list of users who should have permisssions to an NTFS file`n[E] EXAMPLE: 'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators', 'BUILTIN\Network Configuration Operators', 'NT SERVICE\MpsSvc'")]  # End Parameter
-            [Alias('User')]
-            [String[]]$Username,
+[CmdletBinding(
+    DefaultParameterSetName="Local",
+    SupportsShouldProcess,
+    ConfirmImpact='Medium'
+)]  # End CmdletBinding
+    param(
+        [Parameter(
+            Mandatory=$True,
+            ValueFromPipeline=$False,
+            ValueFromPipelineByPropertyName=$False,
+            HelpMessage="`n[H] Add a user or list of users who should have permisssions to an NTFS file`n[E] EXAMPLE: 'NT AUTHORITY\SYSTEM', 'BUILTIN\Administrators'"
+        )]  # End Parameter
+        [Alias('User')]
+        [String[]]$Username,
 
-            [Parameter(
-                Mandatory=$True,
-                ValueFromPipeline=$True,
-                ValueFromPipelineByPropertyName=$False,
-                HelpMessage="`n[H] Define the path to the NTFS item you want to modify the entire permissions on `n[E] EXAMPLE: C:\Temp\file.txt")]  # End Parameter
-            [String[]]$Path,
+        [Parameter(
+            Mandatory=$True,
+            ValueFromPipeline=$True,
+            ValueFromPipelineByPropertyName=$False,
+            HelpMessage="`n[H] Define the path to the NTFS item you want to modify the entire permissions on `n[E] EXAMPLE: C:\Temp\file.txt"
+        )]  # End Parameter
+        [String[]]$Path,
 
-            [Parameter(
-                Mandatory=$False,
-                ValueFromPipeline=$False)]
-            [String]$Owner = 'BUILTIN\Administrators',
+        [Parameter(
+            Mandatory=$False,
+            ValueFromPipeline=$False,
+            ValueFromPipelineByPropertyName=$False
+        )]  # End Parameter
+        [String]$Owner = 'BUILTIN\Administrators',
 
-            [Parameter(
-                ParameterSetName="Remote",
-                Mandatory=$True,
-                ValueFromPipeline=$False,
-                HelpMessage="[H] Enter the computer name(s) you wish to change permissions of a file on"    
-            )]  # End Parameter
-            [Alias('cn')]
-            [String[]]$ComputerName,
-            
-            [Parameter(
-                ParameterSetName="Remote",
-                Mandatory=$False
-            )]  # End Parameter
-            [Switch]$UseSSL
-        )  # End param
+        [Parameter(
+            ParameterSetName="Remote",
+            Mandatory=$False,
+            ValueFromPipeline=$True,
+            ValueFromPipelineByPropertyName=$False
+        )]  # End Parameter
+        [Alias('cn','Computer','c','IPAddress')]
+        [String[]]$ComputerName,
 
-    If ($PSCmdlet.PSBoundParameters -eq "Remote") {
+        [Parameter(
+            ParameterSetName="Remote",
+            Mandatory=$False
+        )]  # End Parameter
+        [Switch]$UseSSL,
+        
+        [Parameter(
+            ParameterSetName="Remote",
+            Mandatory=$False
+        )]  # End Parameter
+        [Switch]$SkipCACheck,
 
-        Invoke-Command -HideComputerName $ComputerName -UseSSL:$UseSSL.IsPresent -ScriptBlock {
+        [Parameter(
+            ParameterSetName="Remote",
+            Mandatory=$False
+        )]  # End Parameter
+        [Switch]$SkipCNCheck,
 
-            Write-Verbose -Message "Modifying access rule proteciton"
-            $Acl = Get-Acl -Path "$Using:Path"
+        [Parameter(
+            ParameterSetName="Remote",
+            Mandatory=$False
+        )]  # End Parameter
+        [Switch]$SkipRevocationCheck 
+    )  # End param
+
+BEGIN {
+
+    Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Set-SecureFilePermission cmdlet executed"
+
+} PROCESS {
+
+    If (!($PSBoundParameters.ContainsKey('ComputerName'))) {
+
+        Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Modifying access rule proteciton on $ComputerName"
+        $Acl = Get-Acl -Path "$Path" -Verbose:$False
+        $Acl.SetAccessRuleProtection($True, $False)
+
+        ForEach ($U in $Username) {
+
+            Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Adding $U permissions to $Path"
+            $Permission = $U, 'FullControl', 'Allow'
+            $AccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $Permission
+            $Acl.AddAccessRule($AccessRule)
+
+        }  # End ForEach
+
+        Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Setting the owner of $Path to $Owner"
+        $Acl.SetOwner((New-Object -TypeName System.Security.Principal.NTAccount("$Owner")))
+        $Acl | Set-Acl -Path "$Path" -Verbose:$False
+
+    } Else {
+
+        Invoke-Command -ArgumentList $Username,$Path,$Owner -HideComputerName $ComputerName -UseSSL:$UseSSL.IsPresent -SessionOption (New-PSSessionOption -SkipCACheck:$SkipCACheck.IsPresent -SkipCNCheck:$SkipCNCheck.IsPresent -SkipRevocationCheck:$SkipRevocationCheck.IsPresent -Verbose:$False) -Port 5986 -ScriptBlock {
+
+            $Username = $Args[0]
+            $Path = $Args[1]
+            $Owner = $Args[2]
+
+            Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Modifying access rule proteciton"
+            $Acl = Get-Acl -Path "$Path" -Verbose:$False
             $Acl.SetAccessRuleProtection($True, $False)
 
-            ForEach ($U in $Using:Username) {
+            ForEach ($U in $Username) {
 
-                Write-Verbose -Message "Adding $U permissions for $Using:Path"
+                Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Adding $U permissions for $Path"
+
                 $Permission = $U, 'FullControl', 'Allow'
                 $AccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $Permission
                 $Acl.AddAccessRule($AccessRule)
 
             }  # End ForEach
 
-            Write-Verbose -Message "Changing the owner of $Using:Path to $Using:Owner"
-            $Acl.SetOwner((New-Object -TypeName System.Security.Principal.NTAccount("$Using:Owner")))
-            $Acl | Set-Acl -Path "$Using:Path"
+            Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Changing the owner of $Path to $Owner"
+            $Acl.SetOwner((New-Object -TypeName System.Security.Principal.NTAccount("$Owner")))
+            $Acl | Set-Acl -Path "$Path" -Verbose:$False
 
-        }  # End Invoke-Command
+        } -Verbose:$False  # End Invoke-Command
 
-    } Else {
-        
-        Write-Verbose -Message "[v] Modifying access rule proteciton"
-        $Acl = Get-Acl -Path "$Path" -Verbose:$False
-        $Acl.SetAccessRuleProtection($True, $False)
-
-        ForEach ($U in $Username) {
-
-            Write-Verbose -Message "[v] Adding $U permissions for $Path"
-            $Permission = @($U, 'FullControl', 'Allow')
-            $AccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $Permission
-            $Acl.AddAccessRule($AccessRule)
-
-        }  # End ForEach
-
-        Write-Verbose -Message "[v] Changing the owner of $Path to $Owner"
-        $Acl.SetOwner((New-Object -TypeName System.Security.Principal.NTAccount("$Owner")))
-        $Acl | Set-Acl -Path "$Path" -Verbose:$False
-        
     }  # End If Else
+
+} END {
+
+    Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Set-SecureFilePermission cmdlet completed execution"
+
+}  # End B P E
 
 }  # End Function Set-SecureFilePermission
