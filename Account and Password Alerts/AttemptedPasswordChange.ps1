2a33bc562d5abd671015335b3f7f8aceb2de3c7f
tobor <rosborne@osbornepro.com>
2026-05-24 10:45:43 -0600

    Update AttemptedPasswordChange.ps1

--- Account and Password Alerts/AttemptedPasswordChange.ps1
+++ Account and Password Alerts/AttemptedPasswordChange.ps1
@@ -1,17 +1,17 @@
+#Requires -Version 3.0
 # Alert IT when a users password is changed
-$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4723 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1
+$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4723 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1 -ErrorAction SilentlyContinue
+$Results = $Event | ForEach-Object -Process {
 
-$Results = $Event | ForEach-Object {
-
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, MachineName, SID, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.User = $_.Properties[0].Value
-            $Obj.MachineName = $_.MachineName
-            $Obj.SID = $_.Properties[2].Value
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "An attempt was made to change an account password"
-
-            $Obj
+    $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, TargetDomain, MachineName, SID, Date, Message
+    $Obj.EventID = $_.Id
+    $Obj.User = $_.Properties[0].Value
+    $Obj.TargetDomain = $_.Properties[1].Value
+    $Obj.MachineName = $_.MachineName
+    $Obj.SID = $_.Properties[2].Value
+    $Obj.Date = $_.TimeCreated
+    $Obj.Message = "An attempt was made to change an account password"
+    Write-Output -InputObject $Obj
 
 }  # End ForEach-Object
 
@@ -21,34 +21,37 @@ If ($Results) {
 <style>
 table {
     font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
+    font-size:11px;
+    color:#333333;
+    border-width: 1px;
+    border-color: #666666;
+    border-collapse: collapse;
 }
 th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #dedede;
 }
 td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #ffffff;
 }
 </style>
 "@ # End CSS
 
-    $PreContent = "<Title>NOTIFICATION: A Password Change Has Been Attempted</Title>"
+    $PreContent = "<h2>NOTIFICATION: A Password Change Has Been Attempted</h2><p>The below table contains information on a password change attempt.</p>"
     $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a user whose password was changed.<br><br><hr><br><br>" | Out-String
-
-    Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Password Change Attempt" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font></p>"
+    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent | Out-String
+    Try {
+        Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Password Change Attempt" -BodyAsHtml -Body $MailBody -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587 -ErrorAction Stop
+    } Catch {
+        Throw "Failed To Send Password Change Alert. $($_.Exception.Message)"
+    }  # End Try Catch
 
 }  # End If
