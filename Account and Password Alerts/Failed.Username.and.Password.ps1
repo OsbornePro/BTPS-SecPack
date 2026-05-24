c38bfbcb23992519359cf8631fc9dcac4eb2262b
tobor <rosborne@osbornepro.com>
2026-05-24 10:47:04 -0600

    Update Failed.Username.and.Password.ps1

--- Account and Password Alerts/Failed.Username.and.Password.ps1
+++ Account and Password Alerts/Failed.Username.and.Password.ps1
@@ -1,21 +1,22 @@
+#Requires -Version 3.0
 # This alert is used to alert IT when a failed password attempt occurs on a server
-$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 120000]]]' -MaxEvents 1
 
-$Results = $Event | ForEach-Object {
+$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4625 and TimeCreated[timediff(@SystemTime) <= 120000]]]' -MaxEvents 1 -ErrorAction SilentlyContinue
+$Results = $Event | ForEach-Object -Process {
 
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, Device, DC, Type, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.User = $_.Properties[5].Value
-            $Obj.Device = $_.Properties[6].Value
-            $Obj.DC = $_.MachineName
-            $Obj.Type = $_.Properties[12].Value
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "An account failed to log on"
+    $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, Device, DC, LogonType, Date, Message
 
-            $Obj
+    $Obj.EventID = $_.Id
+    $Obj.User = $_.Properties[5].Value
+    $Obj.Device = $_.Properties[6].Value
+    $Obj.DC = $_.MachineName
+    $Obj.LogonType = $_.Properties[10].Value
+    $Obj.Date = $_.TimeCreated
+    $Obj.Message = "An account failed to log on"
+
+    Write-Output -InputObject $Obj
 
 }  # End ForEach-Object
-
 
 If ($Results) {
 
@@ -23,34 +24,38 @@ If ($Results) {
 <style>
 table {
     font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
+    font-size:11px;
+    color:#333333;
+    border-width: 1px;
+    border-color: #666666;
+    border-collapse: collapse;
 }
 th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #dedede;
 }
 td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #ffffff;
 }
 </style>
 "@ # End CSS
 
-    $PreContent = "<Title>NOTIFICATION: A Password Change Has Been Attempted</Title>"
+    $PreContent = "<h2>NOTIFICATION: Failed Logon Attempt Detected</h2><p>The below table contains information about a failed logon attempt.</p>"
     $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a user whose password was changed.<br><br><hr><br><br>" | Out-String
+    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font></p>"
+    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent | Out-String
 
-    Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Failed UserName and Password $env:COMPUTERNAME" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+    Try {
+        Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Failed UserName And Password $env:COMPUTERNAME" -BodyAsHtml -Body $MailBody -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587 -ErrorAction Stop
+    } Catch {
+        Throw "Failed To Send Failed Logon Alert. $($_.Exception.Message)"
+    }  # End Try Catch
 
 }  # End If
