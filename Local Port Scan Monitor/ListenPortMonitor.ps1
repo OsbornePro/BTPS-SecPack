7ff4cccf0c8130ed809fc79b6fd6259e139b1d56
tobor <rosborne@osbornepro.com>
2026-05-24 11:54:46 -0600

    Update ListenPortMonitor.ps1

--- Local Port Scan Monitor/ListenPortMonitor.ps1
+++ Local Port Scan Monitor/ListenPortMonitor.ps1
@@ -1,127 +1,145 @@
-﻿###########################################################################################
-#                                                                                         #
-# This shell is for monitoring ports and alerting IT when a new port is opened            #
-#                                                                                         #
-# Author: Robert Osborne                                                                  #
-#                                                                                         #
-# Contact: rosborne@osbornepro.com                                                        #
-#                                                                                         #
-# Last Updated 9/8/2020                                                                  #
-#                                                                                         #
-###########################################################################################
-$DomainInfo = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
-$PDC = $DomainInfo.PdcRoleOwner.Name
-
-# Monitor Bind Shells--------------------------------------------------------------------------
-$PreviouslyOpenPorts = "34"
-$CurrentlyOpenPorts = Get-NetTCPConnection -State Listen | Group-Object -Property LocalPort -NoElement 
-
-Write-Verbose "Comparing current open port count to previously open port count"
-If ($PreviouslyOpenPorts -lt $CurrentlyOpenPorts.Count) {
-
-    $Body = "If you have received this email it is because a new port was opened $nev:COMPUTERNAME. If this was due to a user configuration or new application you may disregard. Otherwise verify that a Bind Shell connection has not been established to this device."
-    Send-MailMessage -From FromEmail -To ToEmail -Body $Body -Subject "AD Event: New Listen Port Opened on $env:COMPUTERNAME" -SmtpServer UseSmtpServer -Priority Normal -Credential $Credential -UseSSL -Port 587
-
-}  # End If
-
-
-Write-Verbose "Logging established connections"
-
-$EstablishedConnections = Get-NetTCPConnection -State Established | Sort-Object -Property RemoteAddress -Unique | Select-Object -Property LocalPort,RemoteAddress,RemotePort,State,AppliedSetting,OwningProcess,CreationTime
-If (!(Test-Path -Path 'C:\Users\Public\Documents\ConnectionHistory.csv')) {
-
-    $EstablishedConnections | Export-Csv -Path 'C:\Users\Public\Documents\ConnectionHistory.csv' -Delimiter ',' -NoTypeInformation
-    $DnsResults = ForEach ($Established in $EstablishedConnections.RemoteAddress) {       
+#Requires -Version 3.0
+#Requires -Modules NetTCPIP
+Function Watch-OpenPorts {
+    [CmdletBinding()]
+        param (
+            [Parameter(
+                Mandatory=$True,
+                Position=0)]
+            [PSCredential]$Credential,
     
-        Resolve-DnsName -Name $Established -Server $PDC -ErrorAction SilentlyContinue | Select-Object -Property Name,Type,NameHost
-
-    } # End ForEach
-
-    $DnsResults | Export-Csv -Path 'C:\Users\Public\Documents\ConnectionDNSHistory.csv' -Delimiter ',' -Append -NoTypeInformation
-
-}# End If
-Else {
-
-    $NewConnections = Compare-Object -ReferenceObject (Import-Csv 'C:\Users\Public\Documents\ConnectionHistory.csv') -DifferenceObject $EstablishedConnections -Property RemoteAddress | Where-Object { $_.SideIndicator -like '=>'} | Select-Object -ExpandProperty RemoteAddress 
-
-    ForEach ($NewConnection in $NewConnections) {
+            [Parameter(
+                Mandatory=$True,
+                Position=1)]
+            [String]$SmtpServer,
     
-        $EstablishedConnections | Where-Object -Property RemoteAddress -like $NewConnection | Export-Csv -Path 'C:\Users\Public\Documents\ConnectionHistory.csv' -Append
-        Resolve-DnsName -Name $NewConnection -ErrorAction SilentlyContinue | Select-Object -Property Name,Type,NameHost | Export-Csv -Path 'C:\Users\Public\Documents\ConnectionDNSHistory.csv' -Append -NoTypeInformation -Delimiter ','
+            [Parameter(
+                Mandatory=$True,
+                Position=2)]
+            [String]$FromEmail,
+    
+            [Parameter(
+                Mandatory=$True,
+                Position=3)]
+            [String]$ToEmail,
+    
+            [Parameter(
+                Mandatory=$False,
+                Position=4)]
+            [String]$OutputPath = "C:\Users\Public\Documents"
+        )  # End Param
 
-    } # End ForEach
+    BEGIN {
 
-} # End Else
+        Clear-Variable -Name DomainInfo,PDC,PreviouslyOpenPorts,CurrentlyOpenPorts,NewOpenPorts,EstablishedConnections,NewConnections,DnsResults -ErrorAction SilentlyContinue
+        $PortHistoryPath = Join-Path -Path $OutputPath -ChildPath "OpenPortHistory.csv"
+        $ConnectionHistoryPath = Join-Path -Path $OutputPath -ChildPath "ConnectionHistory.csv"
+        $ConnectionDNSHistoryPath = Join-Path -Path $OutputPath -ChildPath "ConnectionDNSHistory.csv"
+        If (!(Test-Path -Path $OutputPath)) {
+            New-Item -ItemType Directory -Path $OutputPath -ErrorAction SilentlyContinue | Out-Null
+        }  # End If
 
-# SIG # Begin signature block
-# MIIM9AYJKoZIhvcNAQcCoIIM5TCCDOECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUuC5+byWfkvD/QmwruIOSy/VO
-# F8Cgggn7MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UE
-# BhMCVVMxEDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAY
-# BgNVBAoTEUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290
-# IENlcnRpZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMx
-# MDUwMzA3MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMw
-# EQYDVQQHEwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEt
-# MCsGA1UECxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMw
-# MQYDVQQDEypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0g
-# RzIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYusw
-# ZLiBCGzDBNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz
-# 6ojcnqOvK/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am
-# +GZHY23ecSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1g
-# O7GyQ5HYpDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQW
-# OlDxSq7neTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB
-# 0lL7AgMBAAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIB
-# BjAdBgNVHQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqF
-# BxBnKLbv9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhho
-# dHRwOi8vb2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDov
-# L2NybC5nb2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0g
-# ADAzMDEGCCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9z
-# aXRvcnkvMA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyI
-# BslQj6Zz91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwl
-# TxFWMMS2RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKo
-# cyQetawiDsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1
-# KrKQ0U11GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkK
-# rqeKM+2xLXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDABMIIFIzCC
-# BAugAwIBAgIIXIhNoAmmSAYwDQYJKoZIhvcNAQELBQAwgbQxCzAJBgNVBAYTAlVT
-# MRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQHEwpTY290dHNkYWxlMRowGAYDVQQK
-# ExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UECxMkaHR0cDovL2NlcnRzLmdvZGFk
-# ZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQDEypHbyBEYWRkeSBTZWN1cmUgQ2Vy
-# dGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwHhcNMjAxMTE1MjMyMDI5WhcNMjExMTA0
-# MTkzNjM2WjBlMQswCQYDVQQGEwJVUzERMA8GA1UECBMIQ29sb3JhZG8xGTAXBgNV
-# BAcTEENvbG9yYWRvIFNwcmluZ3MxEzARBgNVBAoTCk9zYm9ybmVQcm8xEzARBgNV
-# BAMTCk9zYm9ybmVQcm8wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJ
-# V6Cvuf47D4iFITUSNj0ucZk+BfmrRG7XVOOiY9o7qJgaAN88SBSY45rpZtGnEVAY
-# Avj6coNuAqLa8k7+Im72TkMpoLAK0FZtrg6PTfJgi2pFWP+UrTaorLZnG3oIhzNG
-# Bt5oqBEy+BsVoUfA8/aFey3FedKuD1CeTKrghedqvGB+wGefMyT/+jaC99ezqGqs
-# SoXXCBeH6wJahstM5WAddUOylTkTEfyfsqWfMsgWbVn3VokIqpL6rE6YCtNROkZq
-# fCLZ7MJb5hQEl191qYc5VlMKuWlQWGrgVvEIE/8lgJAMwVPDwLNcFnB+zyKb+ULu
-# rWG3gGaKUk1Z5fK6YQ+BAgMBAAGjggGFMIIBgTAMBgNVHRMBAf8EAjAAMBMGA1Ud
-# JQQMMAoGCCsGAQUFBwMDMA4GA1UdDwEB/wQEAwIHgDA1BgNVHR8ELjAsMCqgKKAm
-# hiRodHRwOi8vY3JsLmdvZGFkZHkuY29tL2dkaWcyczUtNi5jcmwwXQYDVR0gBFYw
-# VDBIBgtghkgBhv1tAQcXAjA5MDcGCCsGAQUFBwIBFitodHRwOi8vY2VydGlmaWNh
-# dGVzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMAgGBmeBDAEEATB2BggrBgEFBQcB
-# AQRqMGgwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmdvZGFkZHkuY29tLzBABggr
-# BgEFBQcwAoY0aHR0cDovL2NlcnRpZmljYXRlcy5nb2RhZGR5LmNvbS9yZXBvc2l0
-# b3J5L2dkaWcyLmNydDAfBgNVHSMEGDAWgBRAwr0njsw0gzCiM9f7bLPwtCyAzjAd
-# BgNVHQ4EFgQUkWYB7pDl3xX+PlMK1XO7rUHjbrwwDQYJKoZIhvcNAQELBQADggEB
-# AFSsN3fgaGGCi6m8GuaIrJayKZeEpeIK1VHJyoa33eFUY+0vHaASnH3J/jVHW4BF
-# U3bgFR/H/4B0XbYPlB1f4TYrYh0Ig9goYHK30LiWf+qXaX3WY9mOV3rM6Q/JfPpf
-# x55uU9T4yeY8g3KyA7Y7PmH+ZRgcQqDOZ5IAwKgknYoH25mCZwoZ7z/oJESAstPL
-# vImVrSkCPHKQxZy/tdM9liOYB5R2o/EgOD5OH3B/GzwmyFG3CqrqI2L4btQKKhm+
-# CPrue5oXv2theaUOd+IYJW9LA3gvP/zVQhlOQ/IbDRt7BibQp0uWjYaMAOaEKxZN
-# IksPKEJ8AxAHIvr+3P8R17UxggJjMIICXwIBATCBwTCBtDELMAkGA1UEBhMCVVMx
-# EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT
-# EUdvRGFkZHkuY29tLCBJbmMuMS0wKwYDVQQLEyRodHRwOi8vY2VydHMuZ29kYWRk
-# eS5jb20vcmVwb3NpdG9yeS8xMzAxBgNVBAMTKkdvIERhZGR5IFNlY3VyZSBDZXJ0
-# aWZpY2F0ZSBBdXRob3JpdHkgLSBHMgIIXIhNoAmmSAYwCQYFKw4DAhoFAKB4MBgG
-# CisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcC
-# AQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYE
-# FC6fCG5RZwjUsTg62vwhH3XDcsqqMA0GCSqGSIb3DQEBAQUABIIBADh5QbY21CSM
-# zEF4fxVS3aCGmf6FPPCVj6c3cZ2oPssHLhuGGAGjwXS1xyjNJalftLWLs76D4ykc
-# ollbtW3CUPjtTopLGDekEz5mfy0eusGUfwFhoYh5nqrSuaWUqd3fFGtcto8y0AU2
-# AYqqqS4/CXSazpuhXG+qiQqTGlIXh3vwOnfna0bq1uVcD/eMGE7R3wnTTC1Pl3Xv
-# zNBKfFPEmFw134D4jRefzzSbE/jEE11jV/9NUIte1rpNcGsnMw/aFpHdb2H5I9uF
-# Pz4t+s3tMtNgcEzUuMzWdBTGCYGkZEFP+PKDZU+BSVRujw+cAlMgcNwQScxRpxmn
-# MWYrKxSXiVs=
-# SIG # End signature block
+        Try {
+            $DomainInfo = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+            $PDC = $DomainInfo.PdcRoleOwner.Name
+        } Catch {
+            Write-Verbose -Message "[*] Could Not Determine PDC. DNS Lookups Will Use Default Resolver."
+            $PDC = $Null
+        }  # End Try Catch
+
+    } PROCESS {
+
+        Write-Verbose -Message "[*] Getting Currently Listening Ports"
+        $CurrentlyOpenPorts = Get-NetTCPConnection -State Listen | Select-Object -Property LocalAddress,LocalPort,State,OwningProcess | Sort-Object -Property LocalPort -Unique
+        If (!(Test-Path -Path $PortHistoryPath)) {
+
+            Write-Verbose -Message "[*] Creating Initial Open Port Baseline"
+            $CurrentlyOpenPorts | Export-Csv -Path $PortHistoryPath -NoTypeInformation
+
+        } Else {
+
+            Write-Verbose -Message "[*] Comparing Current Open Ports To Previous Baseline"
+            $PreviouslyOpenPorts = Import-Csv -Path $PortHistoryPath
+            $NewOpenPorts = Compare-Object `
+                -ReferenceObject $PreviouslyOpenPorts `
+                -DifferenceObject $CurrentlyOpenPorts `
+                -Property LocalPort |
+                Where-Object -FilterScript {
+                    $_.SideIndicator -eq "=>"
+                } |
+                Select-Object -ExpandProperty LocalPort -Unique
+
+            If ($NewOpenPorts) {
+
+                Write-Output -InputObject "[!] New Listening Port Detected On $env:COMPUTERNAME"
+                $Body = "If you have received this email it is because a new listening port was opened on $env:COMPUTERNAME. If this was due to a user configuration or new application you may disregard. Otherwise verify that a bind shell connection has not been established to this device.`n`nNew Ports:`n$($NewOpenPorts -join "`n")"
+                Try {
+
+                    Send-MailMessage `
+                        -From $FromEmail `
+                        -To $ToEmail `
+                        -Body $Body `
+                        -Subject "AD Event: New Listen Port Opened On $env:COMPUTERNAME" `
+                        -SmtpServer $SmtpServer `
+                        -Priority Normal `
+                        -Credential $Credential `
+                        -UseSSL `
+                        -Port 587 `
+                        -ErrorAction Stop
+                    Write-Verbose -Message "[*] Email Sent"
+
+                } Catch {
+                    Write-Output -InputObject "[x] Failed Sending Open Port Alert Email"
+                    Write-Output -InputObject $_
+                }  # End Try Catch
+                $CurrentlyOpenPorts | Export-Csv -Path $PortHistoryPath -NoTypeInformation
+
+            } Else {
+                Write-Verbose -Message "[*] No New Listening Ports Found"
+            }  # End If Else
+
+        }  # End If Else
+
+        Write-Verbose -Message "[*] Logging Established Connections"
+        $EstablishedConnections = Get-NetTCPConnection -State Established | Sort-Object -Property RemoteAddress -Unique | Select-Object -Property LocalPort,RemoteAddress,RemotePort,State,AppliedSetting,OwningProcess,CreationTime
+        If (!(Test-Path -Path $ConnectionHistoryPath)) {
+
+            Write-Verbose -Message "[*] Creating Initial Connection History"
+            $EstablishedConnections | Export-Csv -Path $ConnectionHistoryPath -Delimiter ',' -NoTypeInformation
+            $DnsResults = ForEach ($Established in $EstablishedConnections.RemoteAddress) {
+
+                If ($PDC) {
+                    Resolve-DnsName -Name $Established -Server $PDC -ErrorAction SilentlyContinue | Select-Object -Property Name,Type,NameHost
+                } Else {
+                    Resolve-DnsName -Name $Established -ErrorAction SilentlyContinue | Select-Object -Property Name,Type,NameHost
+                }  # End If Else
+
+            }  # End ForEach
+
+            $DnsResults | Export-Csv -Path $ConnectionDNSHistoryPath -Delimiter ',' -NoTypeInformation
+
+        } Else {
+
+            Write-Verbose -Message "[*] Comparing Established Connections To Previous History"
+            $NewConnections = Compare-Object -ReferenceObject (Import-Csv -Path $ConnectionHistoryPath) -DifferenceObject $EstablishedConnections -Property RemoteAddress | Where-Object -FilterScript {
+                    $_.SideIndicator -eq "=>"
+            } | Select-Object -ExpandProperty RemoteAddress -Unique
+
+            ForEach ($NewConnection in $NewConnections) {
+
+                $EstablishedConnections | Where-Object -Property RemoteAddress -Like $NewConnection | Export-Csv -Path $ConnectionHistoryPath -Append -NoTypeInformation
+                If ($PDC) {
+                    Resolve-DnsName -Name $NewConnection -Server $PDC -ErrorAction SilentlyContinue | Select-Object -Property Name,Type,NameHost | Export-Csv -Path $ConnectionDNSHistoryPath -Append -NoTypeInformation -Delimiter ','
+                } Else {
+                    Resolve-DnsName -Name $NewConnection -ErrorAction SilentlyContinue | Select-Object -Property Name,Type,NameHost | Export-Csv -Path $ConnectionDNSHistoryPath -Append -NoTypeInformation -Delimiter ','
+                }  # End If Else
+
+            }  # End ForEach
+
+        }  # End If Else
+
+    } END {
+    
+    }  # End B P E
+
+}  # End Function Watch-OpenPorts
