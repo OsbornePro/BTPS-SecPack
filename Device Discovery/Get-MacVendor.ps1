a145330cd4769e067fe301bded4d7a25efb95daf
tobor <rosborne@osbornepro.com>
2026-05-24 11:30:19 -0600

    Update Get-MacVendor.ps1

--- Device Discovery/Get-MacVendor.ps1
+++ Device Discovery/Get-MacVendor.ps1
@@ -1,142 +1,190 @@
-﻿Function Get-MACVendor {
+<#
+.SYNOPSIS
+This cmdlet is for identifying when an unusual service is being run, possibly indicating credentials were compromised.
+
+
+.DESCRIPTION
+This is best used as a task that runs in response to Event ID 7009 or 7045.
+The newest matching System events are collected and sent to administrators as an HTML email alert.
+Windows Defender definition update service events are excluded to reduce noise.
+
+
+.PARAMETER SmtpServer
+Defines the SMTP server used to send the alert email.
+
+.PARAMETER To
+Defines the email address receiving the alert.
+
+.PARAMETER From
+Defines the email address sending the alert.
+
+.PARAMETER Credential
+Defines the PSCredential object used to authenticate to the SMTP server.
+
+
+.EXAMPLE
+PS> Get-NewlyInstalledService `
+    -SmtpServer mail.smtp2go.com `
+    -To alert@osbornepro.com `
+    -From alerter@osbornepro.com `
+    -Credential (Get-Credential) `
+    -Verbose
+# This example sends an alert email when matching service-related event IDs are discovered.
+
+
+.INPUTS
+None.
+
+
+.OUTPUTS
+None.
+
+
+.NOTES
+Author: Robert H. Osborne
+Alias: tobor
+Contact: rosborne@osbornepro.com
+
+
+.LINK
+https://osbornepro.com
+#>
+Function Get-NewlyInstalledService {
     [CmdletBinding()]
-    param(
-        [Parameter(
-            Position=0,
-            Mandatory=$true,
-            HelpMessage='MAC-Address or the first 6 digits of it')]
-
-        [ValidateScript({
-                If ($_ -match "^(([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9A-Fa-f]{2}){6})|([0-9A-Fa-f]{2}[:-]){2}([0-9A-Fa-f]{2})|([0-9A-Fa-f]{2}){3}$")
-                {
-
-                    Return $true
-
-                }  # End If
-                Else
-                {
-
-                    Throw "Enter a valid MAC-Address (like 00:00:00:00:00:00 or 00-00-00-00-00-00)!"
-
-                }  # End Else
-        })]  # End ValidateScript
-        [String[]]$MACAddress
-
-    )  # End param
+        param(
+            [Parameter(
+                Mandatory=$True,
+                Position=0,
+                HelpMessage="Enter an SMTP Server to use. Example: mail.smtp2go.com"
+            )]  # End Parameter
+            [String]$SmtpServer,
+    
+            [Parameter(
+                Mandatory=$True,
+                Position=1,
+                HelpMessage="Enter an email address to send the alert to. Example: alert@osbornepro.com"
+            )]  # End Parameter
+            [String]$To,
+    
+            [Parameter(
+                Mandatory=$True,
+                Position=2,
+                HelpMessage="Enter an email address to send the alert from. Example: alert@osbornepro.com"
+            )]  # End Parameter
+            [String]$From,
+    
+            [Parameter(
+                Mandatory=$True,
+                Position=3
+            )]  # End Parameter
+            [PSCredential]$Credential,
+    
+            [Parameter(
+                Mandatory=$False,
+                Position=4
+            )]  # End Parameter
+            [Int]$MaxEvents = 2
+        )  # End param
 
     BEGIN {
 
-        # MAC-Vendor list path
-##########################################################################################################################
-        $CSV_MACVendorList_Path = "C:\Users\Public\Documents\PSGetHelp\MAC.Vendor.List.csv"
-        If([System.IO.File]::Exists($CSV_MACVendorList_Path)) {
+        Write-Verbose -Message "[*] Pulling Events In Question"
+        Try {
 
-            $MAC_VendorList = Import-Csv -Path $CSV_MACVendorList_Path | Select-Object -Property "Assignment", "Organization Name"
-            #### The above values may change depending on your csv file. Just replaec Assignment and Organization Name with whatever the headers are in your
+            $EventInfo = Get-WinEvent -FilterHashtable @{
+                    LogName = "System"
+                    ID      = 7009,7045
+                } -MaxEvents $MaxEvents -ErrorAction Stop
+
+        } Catch {
+            Write-Output -InputObject "[x] Failed Pulling Events In Question"
+            Write-Output -InputObject $_
+            Return
+        }  # End Try Catch
+
+    } PROCESS {
+
+        If (!($EventInfo)) {
+            Write-Verbose -Message "[*] No Matching Events Were Found"
+            Return
         }  # End If
-        Else {
 
-            Throw [System.IO.FileNotFoundException] "No CSV-File to assign vendor with MAC-Address found!"
+        Write-Verbose -Message "[*] Filtering Windows Defender Definition Update Events"
+        $EventInfo = $EventInfo | Where-Object -FilterScript {
+            $_.Message -notlike "*Service File Name:  C:\ProgramData\Microsoft\Windows Defender\Definition Updates\*"
+        }  # End Where-Object
 
-        }  # End Else
+        If (!($EventInfo)) {
+            Write-Verbose -Message "[*] Only Windows Defender Definition Update Events Were Found"
+            Return
+        }  # End If
+        Write-Verbose -Message "[*] Converting Event Into HTML Viewable Format"
 
-    }  # End BEGIN
-    PROCESS {
+$Css = @"
+<style>
+table {
+    font-family: verdana,arial,sans-serif;
+    font-size:11px;
+    color:#333333;
+    border-width:1px;
+    border-color:#666666;
+    border-collapse:collapse;
+}
+th {
+    border-width:1px;
+    padding:8px;
+    border-style:solid;
+    border-color:#666666;
+    background-color:#dedede;
+}
+td {
+    border-width:1px;
+    padding:8px;
+    border-style:solid;
+    border-color:#666666;
+    background-color:#ffffff;
+}
+</style>
+"@
 
-        ForEach ($MACAddress2 in $MACAddress) {
+        Write-Verbose -Message "[*] Building Email Mail Body"
+        $TableInfo = $EventInfo | Select-Object -Property TimeCreated,Id,ProviderName,ProcessId,UserId,MachineName,Message
 
-            $Vendor = [String]::Empty
-            # Split it, so we can search the vendor (XX-XX-XX-XX-XX-XX to XX-XX-XX)
-            $MAC_VendorSearch = $MACAddress2.Replace("-","").Replace(":","").Substring(0,6)
-            ForEach ($ListEntry in $MAC_VendorList) {
+        $PreContent = "<Title>MITM Monitoring Alert: Watches for Newly Installed Services</Title>"
+        $NoteLine = "$(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
+        $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
+        $MailBody = $TableInfo |
+                    ConvertTo-Html `
+                        -Head $Css `
+                        -PostContent $PostContent `
+                        -PreContent $PreContent `
+                        -Body "If you see BTOBTO, Base64 encoded service names, or random characters for a service name the attacker may have SYSTEM PRIVILEGE. This may also mean admin credentials have been compromised." |
+                    Out-String
 
-                If ($ListEntry.Assignment -eq $MAC_VendorSearch) {
+        Write-Verbose -Message "[*] Creating Email Body And Placing Info Into A Neat Looking Table"
+        Try {
 
-                    $Vendor = $ListEntry."Organization Name"
-                    [pscustomobject] @{
-                        ClientId = $MACAddress2
-                        Vendor = $Vendor
-                    }  # End CustomObject
+            Send-MailMessage `
+                -From $From `
+                -To $To `
+                -Subject "$env:COMPUTERNAME Had New Service Installed" `
+                -BodyAsHtml `
+                -Body $MailBody `
+                -SmtpServer $SmtpServer `
+                -Credential $Credential `
+                -UseSSL `
+                -Port 587 `
+                -ErrorAction Stop
 
-                }  # End If
+            Write-Verbose -Message "[*] Email Sent"
 
-            }  # End ForEach
+        } Catch {
+            Write-Output -InputObject "[x] Failed Sending Email"
+            Write-Output -InputObject $_
+        }  # End Try Catch
 
-        }  # End ForEach
+    } END {
+    
+    }  # End B P E
 
-    }  # End PROCESS
-
-}  # End Function Get-MacVendor
-
-# SIG # Begin signature block
-# MIIM9AYJKoZIhvcNAQcCoIIM5TCCDOECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUQ4wT5NQ89jEFofCPznN2Ski0
-# M8qgggn7MIIE0DCCA7igAwIBAgIBBzANBgkqhkiG9w0BAQsFADCBgzELMAkGA1UE
-# BhMCVVMxEDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAY
-# BgNVBAoTEUdvRGFkZHkuY29tLCBJbmMuMTEwLwYDVQQDEyhHbyBEYWRkeSBSb290
-# IENlcnRpZmljYXRlIEF1dGhvcml0eSAtIEcyMB4XDTExMDUwMzA3MDAwMFoXDTMx
-# MDUwMzA3MDAwMFowgbQxCzAJBgNVBAYTAlVTMRAwDgYDVQQIEwdBcml6b25hMRMw
-# EQYDVQQHEwpTY290dHNkYWxlMRowGAYDVQQKExFHb0RhZGR5LmNvbSwgSW5jLjEt
-# MCsGA1UECxMkaHR0cDovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMTMw
-# MQYDVQQDEypHbyBEYWRkeSBTZWN1cmUgQ2VydGlmaWNhdGUgQXV0aG9yaXR5IC0g
-# RzIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQC54MsQ1K92vdSTYusw
-# ZLiBCGzDBNliF44v/z5lz4/OYuY8UhzaFkVLVat4a2ODYpDOD2lsmcgaFItMzEUz
-# 6ojcnqOvK/6AYZ15V8TPLvQ/MDxdR/yaFrzDN5ZBUY4RS1T4KL7QjL7wMDge87Am
-# +GZHY23ecSZHjzhHU9FGHbTj3ADqRay9vHHZqm8A29vNMDp5T19MR/gd71vCxJ1g
-# O7GyQ5HYpDNO6rPWJ0+tJYqlxvTV0KaudAVkV4i1RFXULSo6Pvi4vekyCgKUZMQW
-# OlDxSq7neTOvDCAHf+jfBDnCaQJsY1L6d8EbyHSHyLmTGFBUNUtpTrw700kuH9zB
-# 0lL7AgMBAAGjggEaMIIBFjAPBgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIB
-# BjAdBgNVHQ4EFgQUQMK9J47MNIMwojPX+2yz8LQsgM4wHwYDVR0jBBgwFoAUOpqF
-# BxBnKLbv9r0FQW4gwZTaD94wNAYIKwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhho
-# dHRwOi8vb2NzcC5nb2RhZGR5LmNvbS8wNQYDVR0fBC4wLDAqoCigJoYkaHR0cDov
-# L2NybC5nb2RhZGR5LmNvbS9nZHJvb3QtZzIuY3JsMEYGA1UdIAQ/MD0wOwYEVR0g
-# ADAzMDEGCCsGAQUFBwIBFiVodHRwczovL2NlcnRzLmdvZGFkZHkuY29tL3JlcG9z
-# aXRvcnkvMA0GCSqGSIb3DQEBCwUAA4IBAQAIfmyTEMg4uJapkEv/oV9PBO9sPpyI
-# BslQj6Zz91cxG7685C/b+LrTW+C05+Z5Yg4MotdqY3MxtfWoSKQ7CC2iXZDXtHwl
-# TxFWMMS2RJ17LJ3lXubvDGGqv+QqG+6EnriDfcFDzkSnE3ANkR/0yBOtg2DZ2HKo
-# cyQetawiDsoXiWJYRBuriSUBAA/NxBti21G00w9RKpv0vHP8ds42pM3Z2Czqrpv1
-# KrKQ0U11GIo/ikGQI31bS/6kA1ibRrLDYGCD+H1QQc7CoZDDu+8CL9IVVO5EFdkK
-# rqeKM+2xLXY2JtwE65/3YR8V3Idv7kaWKK2hJn0KCacuBKONvPi8BDABMIIFIzCC
-# BAugAwIBAgIIXIhNoAmmSAYwDQYJKoZIhvcNAQELBQAwgbQxCzAJBgNVBAYTAlVT
-# MRAwDgYDVQQIEwdBcml6b25hMRMwEQYDVQQHEwpTY290dHNkYWxlMRowGAYDVQQK
-# ExFHb0RhZGR5LmNvbSwgSW5jLjEtMCsGA1UECxMkaHR0cDovL2NlcnRzLmdvZGFk
-# ZHkuY29tL3JlcG9zaXRvcnkvMTMwMQYDVQQDEypHbyBEYWRkeSBTZWN1cmUgQ2Vy
-# dGlmaWNhdGUgQXV0aG9yaXR5IC0gRzIwHhcNMjAxMTE1MjMyMDI5WhcNMjExMTA0
-# MTkzNjM2WjBlMQswCQYDVQQGEwJVUzERMA8GA1UECBMIQ29sb3JhZG8xGTAXBgNV
-# BAcTEENvbG9yYWRvIFNwcmluZ3MxEzARBgNVBAoTCk9zYm9ybmVQcm8xEzARBgNV
-# BAMTCk9zYm9ybmVQcm8wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJ
-# V6Cvuf47D4iFITUSNj0ucZk+BfmrRG7XVOOiY9o7qJgaAN88SBSY45rpZtGnEVAY
-# Avj6coNuAqLa8k7+Im72TkMpoLAK0FZtrg6PTfJgi2pFWP+UrTaorLZnG3oIhzNG
-# Bt5oqBEy+BsVoUfA8/aFey3FedKuD1CeTKrghedqvGB+wGefMyT/+jaC99ezqGqs
-# SoXXCBeH6wJahstM5WAddUOylTkTEfyfsqWfMsgWbVn3VokIqpL6rE6YCtNROkZq
-# fCLZ7MJb5hQEl191qYc5VlMKuWlQWGrgVvEIE/8lgJAMwVPDwLNcFnB+zyKb+ULu
-# rWG3gGaKUk1Z5fK6YQ+BAgMBAAGjggGFMIIBgTAMBgNVHRMBAf8EAjAAMBMGA1Ud
-# JQQMMAoGCCsGAQUFBwMDMA4GA1UdDwEB/wQEAwIHgDA1BgNVHR8ELjAsMCqgKKAm
-# hiRodHRwOi8vY3JsLmdvZGFkZHkuY29tL2dkaWcyczUtNi5jcmwwXQYDVR0gBFYw
-# VDBIBgtghkgBhv1tAQcXAjA5MDcGCCsGAQUFBwIBFitodHRwOi8vY2VydGlmaWNh
-# dGVzLmdvZGFkZHkuY29tL3JlcG9zaXRvcnkvMAgGBmeBDAEEATB2BggrBgEFBQcB
-# AQRqMGgwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmdvZGFkZHkuY29tLzBABggr
-# BgEFBQcwAoY0aHR0cDovL2NlcnRpZmljYXRlcy5nb2RhZGR5LmNvbS9yZXBvc2l0
-# b3J5L2dkaWcyLmNydDAfBgNVHSMEGDAWgBRAwr0njsw0gzCiM9f7bLPwtCyAzjAd
-# BgNVHQ4EFgQUkWYB7pDl3xX+PlMK1XO7rUHjbrwwDQYJKoZIhvcNAQELBQADggEB
-# AFSsN3fgaGGCi6m8GuaIrJayKZeEpeIK1VHJyoa33eFUY+0vHaASnH3J/jVHW4BF
-# U3bgFR/H/4B0XbYPlB1f4TYrYh0Ig9goYHK30LiWf+qXaX3WY9mOV3rM6Q/JfPpf
-# x55uU9T4yeY8g3KyA7Y7PmH+ZRgcQqDOZ5IAwKgknYoH25mCZwoZ7z/oJESAstPL
-# vImVrSkCPHKQxZy/tdM9liOYB5R2o/EgOD5OH3B/GzwmyFG3CqrqI2L4btQKKhm+
-# CPrue5oXv2theaUOd+IYJW9LA3gvP/zVQhlOQ/IbDRt7BibQp0uWjYaMAOaEKxZN
-# IksPKEJ8AxAHIvr+3P8R17UxggJjMIICXwIBATCBwTCBtDELMAkGA1UEBhMCVVMx
-# EDAOBgNVBAgTB0FyaXpvbmExEzARBgNVBAcTClNjb3R0c2RhbGUxGjAYBgNVBAoT
-# EUdvRGFkZHkuY29tLCBJbmMuMS0wKwYDVQQLEyRodHRwOi8vY2VydHMuZ29kYWRk
-# eS5jb20vcmVwb3NpdG9yeS8xMzAxBgNVBAMTKkdvIERhZGR5IFNlY3VyZSBDZXJ0
-# aWZpY2F0ZSBBdXRob3JpdHkgLSBHMgIIXIhNoAmmSAYwCQYFKw4DAhoFAKB4MBgG
-# CisGAQQBgjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcC
-# AQQwHAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYE
-# FLuRgd9j6g/LCkDEwOqrNEVWLJnnMA0GCSqGSIb3DQEBAQUABIIBACEvARwyLBQc
-# 876XjBziVA5C98nSKUFKWTB5xIjxOzdCBuQdWp1df9+q/1gHqCexW+0GzJ+Ko1dZ
-# NS+p4prBbgWain2Pu6LV3h1dOEatUo2u084Nj77MieYs3BJ7myH3VYs/442iItTD
-# 6mEpJIS06lJ6IHWayx5WyYBI0E9L1dFiDxnv5E0FSlxhMlioIHqr4k554aUj9lRL
-# 6WG8FvdVqeF8wk/+iUq+wmxFDHVUOk7FpWcN45KnjxDqAdcfRAri9PQLK1eO1w+6
-# TCC2bRq2Ijqi5kCm7HKhoP/XO+1qYfO7dSAUJpNhVYAbXigEm1BlKpZjwtmri44X
-# pyTO0dIWQbc=
-# SIG # End signature block
+}  # End Function Get-NewlyInstalledService
