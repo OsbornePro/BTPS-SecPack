d34a9aa804c7b6d3f52fb49261d3ebd268ca266f
tobor <rosborne@osbornepro.com>
2026-05-24 10:43:35 -0600

    Update AccountsExpiringCheck.ps1

--- Account and Password Alerts/AccountsExpiringCheck.ps1
+++ Account and Password Alerts/AccountsExpiringCheck.ps1
@@ -1,8 +1,21 @@
+#Requires -Version 3.0
+#Requires -Modules ActiveDirectory
 # This is an alert for IT whenever an account Expires
-$Accounts = Search-ADAccount -AccountExpiring -TimeSpan "10.00:00:00" | Select-Object -Property AccountExpirationDate, Name, @{
-                                                                                                   Label = "Manager"
-                                                                                                   E = { (Get-Aduser(Get-AdUser $_ -Property Manager).Manager).Name }
-                                                                                                   } # End Properties
+$Accounts = Search-ADAccount -UsersOnly -AccountExpiring -TimeSpan "10.00:00:00" | `
+    Get-ADUser -Properties AccountExpirationDate, Manager | Select-Object -Property AccountExpirationDate, Name, SamAccountName, @{
+        Label = "Manager"
+        E = {
+            If ($_.Manager) {
+                Try {
+                    (Get-ADUser -Identity $_.Manager -Properties DisplayName -ErrorAction Stop).DisplayName
+                } Catch {
+                    Write-Output -InputObject "Manager Lookup Failed"
+                }  # End Try Catch
+            } Else {
+                Write-Output -InputObject "No Manager Listed"
+            }  # End If Else
+        }  # End E
+    } | Sort-Object -Property AccountExpirationDate
 
 If ($Accounts) {
 
@@ -10,34 +23,37 @@ If ($Accounts) {
 <style>
 table {
     font-family: verdana,arial,sans-serif;
-	font-size:11px;
-	color:#333333;
-	border-width: 1px;
-	border-color: #666666;
-	border-collapse: collapse;
+    font-size:11px;
+    color:#333333;
+    border-width: 1px;
+    border-color: #666666;
+    border-collapse: collapse;
 }
 th {
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #666666;
-	background-color: #dedede;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #dedede;
 }
 td {
-	border-width: 1px;
-	padding: 8px;
-	border-style: solid;
-	border-color: #666666;
-	background-color: #ffffff;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #ffffff;
 }
 </style>
 "@
 
-    $PreContent = "<Title>Expiring Users (Next 10 Days)</Title>"
+    $PreContent = "<h2>Expiring Users (Next 10 Days)</h2><p>The below table displays accounts that are expiring soon.</p>"
     $NoteLine = "$(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $Body = $Accounts | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "The below table displays accounts that are expiring soon."  | Out-String
-
-    Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Accounts Expiring" -BodyAsHtml -Body $Body -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font></p>"
+    $Body = $Accounts | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent | Out-String
+    Try {
+        Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Accounts Expiring" -BodyAsHtml -Body $Body -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587 -ErrorAction Stop
+    } Catch {
+        Throw "Failed To Send Expiring Account Alert. $($_.Exception.Message)"
+    }  # End Try Catch
 
 } # End If
