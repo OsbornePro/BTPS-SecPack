17e8d5daa02e95933c0f82abc51df017d9beb2f2
tobor <rosborne@osbornepro.com>
2026-05-24 11:07:20 -0600

    Update Install.ps1

--- AutoRunsToWinEvent/Install.ps1
+++ AutoRunsToWinEvent/Install.ps1
@@ -1,45 +1,39 @@
-Write-Verbose -Message "[v] Create Program Files directories for Autoruns"
+#Requires -Version 5.1
+#Requires -RunAsAdministrator
+[CmdletBinding()]
+	param ()  # End param
+
 $AutoRunsDir = "$env:ProgramFiles\AutorunsToWinEventLog"
 $TaskName = "AutorunsToWinEventLog"
+
 New-Item -Path $AutoRunsDir -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null
 
-Write-Verbose -Message "[v] Download Autorunsc64.exe if it doesn't exist"
-$OSArchitecture = (Get-CimInstance -ClassName Win32_OperatingSystem -Verbose:$False).OSArchitecture
+$OSArchitecture = (Get-CimInstance -ClassName Win32_OperatingSystem).OSArchitecture
 $AutorunsExecutable = "Autorunsc64.exe"
+
 If ($OSArchitecture -notmatch "64") {
-
     $AutorunsExecutable = "Autorunsc.exe"
-
 }  # End If
 
-$AutorunsPath = "$($AutoRunsDir)\$($AutorunsExecutable)"
-If (!(Test-Path -Path $AutoRunsPath)) {
-
-  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-  Invoke-WebRequest -Uri "https://live.sysinternals.com/$($AutorunsExecutable)" -OutFile $AutoRunsPath -Method GET -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox
-
+$AutorunsPath = Join-Path -Path $AutoRunsDir -ChildPath $AutorunsExecutable
+If (!(Test-Path -Path $AutorunsPath)) {
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri "https://live.sysinternals.com/$AutorunsExecutable" -OutFile $AutorunsPath -Method GET -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::FireFox)
 }  # End If
 
-Remove-Item -Path "$AutorunsDir\AutorunsToWinEventLog.ps1" -Force -ErrorAction SilentlyContinue | Out-Null
-Start-Sleep -Seconds 1
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OsbornePro/BTPS-SecPack/master/AutoRunsToWinEvent/AutorunsToWinEventLog.ps1" -Method GET -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile "$AutorunsDir\AutorunsToWinEventLog.ps1"
+Remove-Item -Path "$AutoRunsDir\AutorunsToWinEventLog.ps1" -Force -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OsbornePro/BTPS-SecPack/master/AutoRunsToWinEvent/AutorunsToWinEventLog.ps1" -Method GET -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::FireFox) -OutFile "$AutoRunsDir\AutorunsToWinEventLog.ps1"
 
-$Action = New-ScheduledTaskAction -Execute powershell -Argument "-NoLogo -NonInteractive -WindowStyle Hidden $AutorunsDir\AutorunsToWinEventLog.ps1"
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoLogo -NonInteractive -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$AutoRunsDir\AutorunsToWinEventLog.ps1`""
 $Trigger = New-ScheduledTaskTrigger -Daily -At 10am
 $Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel Highest -LogonType ServiceAccount
-
 Try {
-
-	Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Principal $Principal
-
+    Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -Principal $Principal -ErrorAction Stop
 } Catch [Microsoft.Management.Infrastructure.CimException] {
-
-	Write-Verbose -Message "[v] Task already exists"
-
+    Write-Verbose -Message "[v] $(Get-Date -Format 'MM-dd-yyyy hh:mm:ss') Scheduled task already exists"
 } Catch {
-
-    $Error[0]
-
+    Throw $_
 }  # End Try Catch Catch
+
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden -ExecutionTimeLimit (New-TimeSpan -Minutes 60) -RestartCount 1 -StartWhenAvailable
 Set-ScheduledTask -TaskName $TaskName -Settings $Settings
