267a1f1080ee3d1258cc2b7b651442d6337d9c2f
tobor <rosborne@osbornepro.com>
2026-05-24 10:46:37 -0600

    Update AttemptedPasswordReset.ps1

--- Account and Password Alerts/AttemptedPasswordReset.ps1
+++ Account and Password Alerts/AttemptedPasswordReset.ps1
@@ -1,17 +1,24 @@
+#Requires -Version 3.0
 # Alert IT when a users password is changed by another account
-$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4724 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1
+$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4724 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1 -ErrorAction SilentlyContinue
+$Results = $Event | ForEach-Object -Process {
 
-$Results = $Event | ForEach-Object {
+    $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, AffectedUser, ExecutingUser, MachineName, Date, Message
 
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, EffectedUser, ExecutingUser, MachineName, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.EffectedUser = "$(If ($_.Properties[0].Value -like '') { $Event[0].Properties[4].Value.Replace('$','') } Else { $_.Properties[0].Value })"
-            $Obj.ExecutingUser = $_.Properties[4].Value
-            $Obj.MachineName = $_.MachineName
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "$(If ($_.Properties[0].Value -like '') { 'An Computer System account reset its password' } ElseIf ($_.Properties[4].Value -like "MSOL_*") { 'A user used the Azure to reset their password' } Else { 'An attempt was made to change an account password' })"
+    $Obj.EventID = $_.Id
+    $Obj.AffectedUser = If ([String]::IsNullOrWhiteSpace($_.Properties[0].Value)) { $_.Properties[4].Value.Replace('$','') } Else { $_.Properties[0].Value }
+    $Obj.ExecutingUser = $_.Properties[4].Value
+    $Obj.MachineName = $_.MachineName
+    $Obj.Date = $_.TimeCreated
 
-            $Obj
+    If ([String]::IsNullOrWhiteSpace($_.Properties[0].Value)) {
+        $Obj.Message = "A Computer System account reset its password"
+    } ElseIf ($_.Properties[4].Value -Like "MSOL_*") {
+        $Obj.Message = "A user used Azure to reset their password"
+    } Else {
+        $Obj.Message = "An attempt was made to reset an account password"
+    }  # End If Else
+    Write-Output -InputObject $Obj
 
 }  # End ForEach-Object
 
@@ -21,33 +28,37 @@ If ($Results) {
 <style>
 table {
     font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
+    font-size:11px;
+    color:#333333;
+    border-width: 1px;
+    border-color: #666666;
+    border-collapse: collapse;
 }
 th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #dedede;
 }
 td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #ffffff;
 }
 </style>
 "@ # End CSS
 
-    $PreContent = "<Title>NOTIFICATION: An account has attempted to change the password of another account</Title>"
-    $PostContent = "<br><p><font size='2'><i>This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a user whose password was attempted to be changed by another user.<br><br><hr><br><br>" | Out-String
-
-    Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: User Attempted to Change Other Users Password" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587
+    $PreContent = "<h2>NOTIFICATION: An Account Has Attempted To Reset Another Account Password</h2><p>The below table contains information on a password reset attempt.</p>"
+    $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
+    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font></p>"
+    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent | Out-String
+    Try {
+        Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: User Attempted To Reset Another Users Password" -BodyAsHtml -Body $MailBody -SmtpServer UseSmtpServer -Credential $Credential -UseSSL -Port 587 -ErrorAction Stop
+    } Catch {
+        Throw "Failed To Send Password Reset Alert. $($_.Exception.Message)"
+    }  # End Try Catch
 
 }  # End If
