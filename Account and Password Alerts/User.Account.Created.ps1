c02b74dd505dd02cf3cbfaa3c8e12ca5b4bc5fcf
tobor <rosborne@osbornepro.com>
2026-05-24 10:58:27 -0600

    Update User.Account.Created.ps1

--- Account and Password Alerts/User.Account.Created.ps1
+++ Account and Password Alerts/User.Account.Created.ps1
@@ -1,17 +1,22 @@
+#Requires -Version 3.0
 # Alert IT when a user account is created
-$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4720 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1
 
-$Results = $Event | ForEach-Object {
+$Event = Get-WinEvent -LogName Security -FilterXPath '*[System[EventID=4720 and TimeCreated[timediff(@SystemTime) <= 86400000]]]' -MaxEvents 1 -ErrorAction SilentlyContinue
+$Results = $Event | ForEach-Object -Process {
 
-            $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, User, SID, MachineName, Date, Message
-            $Obj.EventID = $_.Id
-            $Obj.User = $_.Properties[0].Value
-            $Obj.MachineName = $_.MachineName
-            $Obj.SID = $_.Properties[2].Value
-            $Obj.Date = $_.TimeCreated
-            $Obj.Message = "A user account was created"
+    $Obj = New-Object -TypeName PSObject | Select-Object -Property EventID, CreatedUser, CreatedUserDomain, CreatedUserSID, ExecutingUser, ExecutingUserDomain, MachineName, Date, Message
 
-            $Obj
+    $Obj.EventID = $_.Id
+    $Obj.CreatedUser = $_.Properties[0].Value
+    $Obj.CreatedUserDomain = $_.Properties[1].Value
+    $Obj.CreatedUserSID = $_.Properties[2].Value
+    $Obj.ExecutingUser = $_.Properties[4].Value
+    $Obj.ExecutingUserDomain = $_.Properties[5].Value
+    $Obj.MachineName = $_.MachineName
+    $Obj.Date = $_.TimeCreated
+    $Obj.Message = "A user account was created"
+
+    Write-Output -InputObject $Obj
 
 }  # End ForEach-Object
 
@@ -21,34 +26,38 @@ If ($Results) {
 <style>
 table {
     font-family: verdana,arial,sans-serif;
-        font-size:11px;
-        color:#333333;
-        border-width: 1px;
-        border-color: #666666;
-        border-collapse: collapse;
+    font-size:11px;
+    color:#333333;
+    border-width: 1px;
+    border-color: #666666;
+    border-collapse: collapse;
 }
 th {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #dedede;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #dedede;
 }
 td {
-        border-width: 1px;
-        padding: 8px;
-        border-style: solid;
-        border-color: #666666;
-        background-color: #ffffff;
+    border-width: 1px;
+    padding: 8px;
+    border-style: solid;
+    border-color: #666666;
+    background-color: #ffffff;
 }
 </style>
 "@ # End CSS
 
-    $PreContent = "<Title>NOTIFICATION: A User Account has been Created</Title>"
+    $PreContent = "<h2>NOTIFICATION: A User Account Has Been Created</h2><p>The below table contains information on a newly created user account.</p>"
     $NoteLine = "This Message was Sent on $(Get-Date -Format 'MM/dd/yyyy HH:mm:ss')"
-    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
-    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent -Body "<br>The below table contains information on a newly created user account.<br><br><hr><br><br>" | Out-String
+    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font></p>"
+    $MailBody = $Results | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent | Out-String
 
-   Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Account Created" -BodyAsHtml -Body "$MailBody" -SmtpServer UseSmtpServer -UseSsl -Port 587 -Credential $Credential
+    Try {
+        Send-MailMessage -From FromEmail -To ToEmail -Subject "AD Event: Account Created" -BodyAsHtml -Body $MailBody -SmtpServer UseSmtpServer -UseSsl -Port 587 -Credential $Credential -ErrorAction Stop
+    } Catch {
+        Throw "Failed To Send Account Created Alert. $($_.Exception.Message)"
+    }  # End Try Catch
 
 }  # End If
